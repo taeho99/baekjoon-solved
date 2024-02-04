bb95219e7ee59e31d5bfdcab8e47325efb4d2312
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-02-05 02:00:53 +0900

    [Silver II] Title: DNA 비밀번호, Time: 456 ms, Memory: 30796 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -1,64 +1,33 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-    static int[] require; //  {‘A’, ‘C’, ‘G’, ‘T’}
+    static int[] require;
     static int[] now;
-    static String s;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int sLen = Integer.parseInt(st.nextToken());
-        int pLen = Integer.parseInt(st.nextToken());
-
-        s = br.readLine();
-        int left = 0;
-        int right = pLen - 1;
-        st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int s = input.nextInt();
+        int p = input.nextInt();
+        char[] dna = input.next().toCharArray();
         require = new int[4];
-        now = new int[4];
+        now = new int[20];
         for(int i=0; i<4; i++) {
-            require[i] = Integer.parseInt(st.nextToken());
+            require[i] = input.nextInt();
         }
 
         int result = 0;
-        for(int i = left; i <= right; i++) {
-            nowPlus(i);
+        for(int i = 0; i < p; i++) {
+            now[dna[i] - 'A']++;
         }
         if (check()) result++;
-        for(int i = pLen; i < sLen; i++) {
-            nowMinus(i - pLen);
-            nowPlus(i);
-
+        for(int i = p; i < s; i++) {
+            now[dna[i - p] - 'A']--;
+            now[dna[i] - 'A']++;
             if (check()) result++;
         }
         System.out.println(result);
     }
 
-    static void nowMinus(int i) {
-        switch (s.charAt(i)) {
-            case 'A' -> now[0]--;
-            case 'C' -> now[1]--;
-            case 'G' -> now[2]--;
-            case 'T' -> now[3]--;
-        }
-    }
-
-    static void nowPlus(int i) {
-        switch (s.charAt(i)) {
-            case 'A' -> now[0]++;
-            case 'C' -> now[1]++;
-            case 'G' -> now[2]++;
-            case 'T' -> now[3]++;
-        }
-    }
-
     static boolean check() {
-        for(int i=0; i<4; i++) {
-            if (now[i] < require[i]) return false;
-        }
-        return true;
+        return now[0] >= require[0] && now[2] >= require[1] && now[6] >= require[2] && now[19] >= require[3];
     }
 }
