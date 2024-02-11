1a481e6522e9d717e562850f9c532d81423d9c16
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-02-11 22:52:19 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 708 ms, Memory: 55072 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        List<Point> list = new ArrayList<>();
+        while(n-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            list.add(new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+        }
+        Collections.sort(list);
+        for (Point point : list) {
+            sb.append(point.x).append(' ').append(point.y).append('\n');
+        }
+        System.out.print(sb);
+    }
+
+    static class Point implements Comparable<Point> {
+        int x, y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Point p) {
+            if (this.y == p.y) return this.x - p.x;
+            return this.y - p.y;
+        }
+    }
+}
