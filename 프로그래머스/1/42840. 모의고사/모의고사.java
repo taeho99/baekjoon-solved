08b0035c87df275e88e7669887118064383f01e2
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-03-12 19:20:59 +0900

    [level 1] Title: 모의고사, Time: 4.46 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 class Solution {
-    public List<Integer> solution(int[] answers) {
+    public int[] solution(int[] answers) {
         int[] user0 = {1,2,3,4,5}; // 5개
         int[] user1 = {2,1,2,3,2,4,2,5}; // 8개
         int[] user2 = {3,3,1,1,2,2,4,4,5,5}; // 10개
@@ -12,15 +12,12 @@ class Solution {
             if(answers[i] == user2[i%10]) count[2]++;
         }
         
-        int max = 0;
-        for(int c : count) {
-            if(c > max) max = c;
-        }
+        int max = Arrays.stream(count).max().getAsInt();
         List<Integer> answer = new ArrayList<>();
         for(int i=0; i<count.length; i++) {
             if(max == count[i])
                 answer.add(i+1);
         }
-        return answer;
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
