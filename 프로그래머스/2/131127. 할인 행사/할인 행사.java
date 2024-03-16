6185a0a8ec4b568b94a7ab855daeec6aac37d62f
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-03-16 20:55:18 +0900

    [level 2] Title: 할인 행사, Time: 0.04 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 class Solution {
-    public int solution(String[] want, int[] number, String[] discount) {
+   public int solution(String[] want, int[] number, String[] discount) {
         HashMap<String, Integer> me = new HashMap<>();
         for(int i=0; i<want.length; i++) {
             me.put(want[i], number[i]);
@@ -14,17 +14,13 @@ class Solution {
 
         int result = 0;
         for(int i=0; i<discount.length - 9; i++) {
-            boolean isValid = true;
-            for (Map.Entry<String, Integer> entry : me.entrySet()) {
-                if(entry.getValue() != mart.getOrDefault(entry.getKey(), 0)) {
-                    isValid = false;
-                    break;
-                }
-            }
-            if(isValid) result++;
+            if(me.equals(mart)) result++;
             if(i != discount.length - 10) {
                 mart.put(discount[i], mart.get(discount[i]) - 1);
+                if(mart.get(discount[i]) == 0)
+                    mart.remove(discount[i]);
                 mart.put(discount[i + 10], mart.getOrDefault(discount[i + 10], 0) + 1);
+
             }
         }
         return result;
