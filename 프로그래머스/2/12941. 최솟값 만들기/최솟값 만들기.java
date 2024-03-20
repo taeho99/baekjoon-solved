eddccb7fb47d2ffa33a352bfeb11e6a4dfccfef3
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-03-21 00:49:41 +0900

    [level 2] Title: 최솟값 만들기, Time: 1.27 ms, Memory: 52.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution
+{
+    public int solution(int[] A, int[] B)
+    {
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int sum = 0;
+
+        for(int i=0; i<A.length; i++) {
+            sum += A[i] * B[B.length-i-1];
+        }
+
+        return sum;
+    }
+}
