359d24bea2e486a42af46f0386120f028ce2b7f4
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-04-04 16:40:09 +0900

    [Bronze III] Title: 최댓값, Time: 212 ms, Memory: 17596 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,23 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
-
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		int n[] = new int[9];
-		n[0] = input.nextInt();
-		int min = n[0];
-		for(int i=1; i<n.length; i++) {
-			n[i] = input.nextInt();
-			if(n[i] > min)
-				min = n[i];
-		}
-		System.out.print(min + " ");
-		for(int i=0; i<n.length; i++) {
-			if(min == n[i])
-				System.out.println(i + 1);
-		}
-		
-	}
-
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int max = 0, idx = 0;
+        for(int i=1; i<=9; i++) {
+            int n = input.nextInt();
+            if(n > max) {
+                max = n;
+                idx = i;
+            }
+        }
+        System.out.println(max);
+        System.out.println(idx);
+    }
 }
