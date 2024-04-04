4a8295827a17dbe3810ba4af8b7305ba52ca14f9
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-04-04 22:36:51 +0900

    [level 2] Title: 프로세스, Time: 1.28 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
 
 ### 성능 요약
 
-메모리: 72.6 MB, 시간: 0.61 ms
+메모리: 71.4 MB, 시간: 1.28 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 04일 22:14:45
+2024년 04월 04일 22:36:49
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -3,28 +3,44 @@ import java.util.Queue;
 
 class Solution {
     public int solution(int[] priorities, int location) {
+        int answer = 0;
+
+        //프로세스 대기 큐 초기화
         Queue<Process> queue = new LinkedList<>();
         for(int i=0; i<priorities.length; i++) {
             queue.add(new Process(i, priorities[i]));
         }
-        int time = 0;
+
+        //실행횟수 초기화
+        int count = 0;
+
         while(!queue.isEmpty()) {
+            //실행하려고 하는 프로세스 poll
             Process poll = queue.poll();
+            //우선순위가 높은 프로세스가 큐에 있는가?
             boolean isBig = false;
+
             for (Process p : queue) {
+                //우선순위가 높은 프로세스가 큐에 있을 경우
                 if(p.priority > poll.priority) {
-                    queue.add(poll);
+                    queue.add(poll); //큐 맨 뒤에 현재 프로세스 추가
                     isBig = true;
                     break;
                 }
             }
-            if(isBig) continue;
-            else time++;
 
-            if(poll.num == location)
-                return time;
+            //프로세스가 정상적으로 실행된 경우
+            if(!isBig) {
+                //실행횟수를 +1 한다
+                count++;
+                //현재 프로세스가 location과 같은 경우 answer에 실행횟수를 넣고 while문 중단
+                if (poll.num == location) {
+                    answer = count;
+                    break;
+                }
+            }
         }
-        return time;
+        return answer;
     }
 
     static class Process {
