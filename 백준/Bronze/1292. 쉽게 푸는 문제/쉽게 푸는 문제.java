5b9974c03e2fb6dc29a5ef26d9edaf7cd1d4aaa3
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-04-07 20:14:08 +0900

    [Bronze I] Title: 쉽게 푸는 문제, Time: 204 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        List<Integer> list = new ArrayList<>();
+        for(int i=1; i<=50; i++) {
+            for(int j=1; j<=i; j++) {
+                list.add(i);
+            }
+        }
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int sum = 0;
+        for(int i=a; i<=b; i++) {
+            sum += list.get(i-1);
+        }
+        System.out.println(sum);
+    }
+}
