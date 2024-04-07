be5b5a097afb1cfbbe0dc4248a6bbce59dfc75e2
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-04-07 15:59:34 +0900

    [Bronze II] Title: 진법 변환, Time: 124 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
+        System.out.println(Integer.parseInt(split[0], Integer.parseInt(split[1])));
+    }
+}
