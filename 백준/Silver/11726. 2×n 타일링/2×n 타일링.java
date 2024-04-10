0b1c1d8fcb9de8481eb6e6db9ceda5395f866a8b
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-04-10 21:00:46 +0900

    [Silver III] Title: 2×n 타일링, Time: 212 ms, Memory: 17752 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -2,13 +2,15 @@ import java.util.*;
 
 public class Main {
     static int[] dp = new int[1001];
+
     public static void main(String[] args) {
-        int n = new Scanner(System.in).nextInt();
+        Scanner input = new Scanner(System.in);
         dp[1] = 1;
         dp[2] = 2;
-        for(int i=3; i<=n; i++) {
-            dp[i] = (dp[i-2] + dp[i-1]) % 10007;
+        for(int i=3; i<=1000; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 10007;
         }
-        System.out.print(dp[n]);
+        int n = input.nextInt();
+        System.out.println(dp[n]);
     }
 }
