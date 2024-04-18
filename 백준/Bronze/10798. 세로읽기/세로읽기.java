3647ad41d774af8924eae95b86befabe7428b7ba
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-04-18 17:19:41 +0900

    [Bronze I] Title: 세로읽기, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -6,16 +6,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        char[][] arr = new char[5][15];
+        String[] arr = new String[5];
         for(int i=0; i<5; i++) {
-            arr[i] = br.readLine().toCharArray();
+            arr[i] = br.readLine();
         }
         for(int i=0; i<15; i++) {
             for(int j=0; j<5; j++) {
-                if(i < arr[j].length)
-                    sb.append(arr[j][i]);
+                String s = arr[j];
+                if(i < s.length())
+                    sb.append(s.charAt(i));
             }
         }
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
