4f1517b29ce46d1c983a0100bf4f9cff62ce5c56
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-04-21 17:48:25 +0900

    [Bronze II] Title: 전구, Time: 808 ms, Memory: 48896 KB -BaekjoonHub

--- 백준/Bronze/21918. 전구/전구.java
+++ 백준/Bronze/21918. 전구/전구.java
@@ -1,28 +1,18 @@
-import java.util.Scanner;
-
-public class Main {
+import java.util.*;
+class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        int m = input.nextInt();
-        boolean[] arr = new boolean[n+1];
-        for(int i=1; i<=n; i++)
-            arr[i] = input.nextInt() == 1;
-        while(m-- > 0) {
-            int a = input.nextInt();
-            int b = input.nextInt();
-            int c = input.nextInt();
-            if (a == 1) {
-                arr[b] = c == 1;
-            } else if (a == 2) {
-                for(int i=b; i<=c; i++) arr[i] = !arr[i];
-            } else if (a == 3) {
-                for(int i=b; i<=c; i++) arr[i] = false;
-            } else {
-                for(int i=b; i<=c; i++) arr[i] = true;
-            }
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(),m=sc.nextInt();
+        boolean[] arr=new boolean[n];
+        for(int i=0;i<n;i++) arr[i] = sc.nextInt()==1;
+        while(m-->0) {
+            int a=sc.nextInt(),b=sc.nextInt(),c=sc.nextInt();
+            if (a==1)
+                arr[b-1]=c==1;
+            else if (a==2)
+                for(int i=b-1;i<c;i++) arr[i]=!arr[i];
+            else Arrays.fill(arr,b-1,c,a != 3);
         }
-        for(int i=1; i<=n; i++)
-            System.out.print((arr[i] ? 1 : 0) + " ");
+        for(boolean b:arr) System.out.print((b?"1":"0")+" ");
     }
 }
