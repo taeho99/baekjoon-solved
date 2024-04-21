7170497d749a12d932cba9894ea60af405074eb5
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-04-21 18:25:06 +0900

    [Silver V] Title: 기상캐스터, Time: 136 ms, Memory: 14536 KB -BaekjoonHub

---
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int y = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+        char[][] map = new char[y][x];
+        for(int i=0; i<y; i++) {
+            map[i] = br.readLine().toCharArray();
+            boolean isCloud = false;
+            int block = 0;
+            for(int j=0; j<x; j++) {
+                if(map[i][j] == 'c') {
+                    isCloud = true;
+                    block = 0;
+                    sb.append(0).append(' ');
+                } else {
+                    if(isCloud) {
+                        block++;
+                        sb.append(block).append(' ');
+                    } else {
+                        sb.append(-1).append(' ');
+                    }
+                }
+            }
+            sb.append('\n');
+        }
+        System.out.print(sb);
+    }
+}
