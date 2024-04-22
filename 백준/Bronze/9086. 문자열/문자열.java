2710db6b883cf6347b617c8668dbc679855d8f0c
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-04-22 17:44:56 +0900

    [Bronze V] Title: 문자열, Time: 220 ms, Memory: 18300 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0) {
+            String s = sc.next();
+            System.out.println(s.charAt(0) + "" + s.charAt(s.length() - 1));
+        }
+    }
+}
