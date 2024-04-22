63968bcf49fcbdc992de7f13fadd2d7afd185ad8
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-04-22 22:15:09 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 132 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -12,12 +12,15 @@ public class Main {
         while(t-- > 0) {
             int n = Integer.parseInt(br.readLine());
             Map<String, Integer> map = new HashMap<>();
-            for(int i=0; i<n; i++) {
+            while(n-- > 0) {
                 String key = br.readLine().split(" ")[1];
                 map.put(key, map.getOrDefault(key, 0) + 1);
             }
-            int reduce = map.values().stream().mapToInt(i -> i+1).reduce(1, (a, b) -> a*b);
-            sb.append(reduce-1).append('\n');
+            int result = 1;
+            for (int v : map.values()) {
+                result *= (v+1);
+            }
+            sb.append(result-1).append('\n');
         }
         System.out.print(sb);
     }
