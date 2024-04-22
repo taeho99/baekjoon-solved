4826f43d1a61852ec369fb024d35dd8a51fd6c81
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-04-22 17:38:24 +0900

    [Bronze III] Title: 학점계산, Time: 196 ms, Memory: 17520 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Map<String,String> map=new HashMap<>();
+        map.put("A+","4.3");map.put("A0","4.0"); map.put("A-","3.7");
+        map.put("B+","3.3");map.put("B0","3.0"); map.put("B-","2.7");
+        map.put("C+","2.3");map.put("C0","2.0"); map.put("C-","1.7");
+        map.put("D+","1.3");map.put("D0","1.0"); map.put("D-","0.7");map.put("F","0.0");
+        System.out.print(map.get(new Scanner(System.in).next()));
+    }
+}
