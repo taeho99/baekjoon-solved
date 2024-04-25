122627d8b98dfd00c77496ffc06f62f71104febf
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-04-25 16:21:01 +0900

    [Silver V] Title: 배열 합치기, Time: 1484 ms, Memory: 301448 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -11,19 +11,35 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n+m];
+
+        int[] a = new int[n];
+        int[] b = new int[m];
+
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            a[i] = Integer.parseInt(st.nextToken());
         }
         st = new StringTokenizer(br.readLine());
-        for(int i=n; i<n+m; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for(int i=0; i<m; i++) {
+            b[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arr);
 
-        for (int i : arr)
-            sb.append(i).append(' ');
-        System.out.println(sb);
+        int p1 = 0, p2 = 0;
+        while(p1 < n && p2 < m) {
+            if(a[p1]<=b[p2])
+                sb.append(a[p1++]).append(' ');
+            else
+                sb.append(b[p2++]).append(' ');
+        }
+
+        if(p1 == n) {
+            for(int i=p2; i<m; i++)
+                sb.append(b[i]).append(' ');
+        }
+        if(p2 == m) {
+            for(int i=p1; i<n; i++)
+                sb.append(a[i]).append(' ');
+        }
+        System.out.print(sb);
     }
 }
