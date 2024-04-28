20957e3af7ac7da1fffc8374a6846ed75d0b7774
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-04-29 00:38:55 +0900

    [Bronze V] Title: 큰 수 곱셈, Time: 6152 ms, Memory: 397936 KB -BaekjoonHub

---
+++ 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.java
@@ -0,0 +1,2 @@
+import java.math.*;import java.util.*;
+interface Main{static void main(String[] a){Scanner s=new Scanner(System.in);System.out.print(new BigInteger(s.next()).multiply(new BigInteger(s.next())));}}
