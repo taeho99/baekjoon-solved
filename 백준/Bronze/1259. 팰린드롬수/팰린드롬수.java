70ecef86d53bb76e15f44701f666dce818b204ef
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-04-30 21:10:42 +0900

    [Bronze I] Title: 팰린드롬수, Time: 196 ms, Memory: 17464 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -1,10 +1,10 @@
 import java.util.*;
 class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[]a) {
+        Scanner sc=new Scanner(System.in);
         String s;
         while(!(s = sc.next()).equals("0")) {
-            System.out.println(s.equals(new StringBuilder().append(s).reverse().toString()) ? "yes" : "no");
+            System.out.println(s.contentEquals(new StringBuffer(s).reverse())?"yes":"no");
         }
     }
 }
