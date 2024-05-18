c94a93bc5e1803239d9a36b61fc7cdcf169fd48b
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-05-18 21:55:39 +0900

    [Bronze IV] Title: 과자, Time: 212 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        System.out.println(n*k<=m ? 0 : n*k-m);
+    }
+}
