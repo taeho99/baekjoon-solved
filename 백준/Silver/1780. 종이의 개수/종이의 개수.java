106d9f656a6cf366fe327ad3bdca1db994808cd7
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-05-20 22:13:21 +0900

    [Silver II] Title: 종이의 개수, Time: 900 ms, Memory: 310000 KB -BaekjoonHub

---
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][] arr;
+    static int[] result = new int[3];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        arr = new int[n][n];
+        for(int i=0; i<n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        recur(0, 0, n);
+        for (int i : result) {
+            System.out.println(i);
+        }
+    }
+
+    private static void recur(int y, int x, int n) {
+        boolean check = true;
+        int color = arr[y][x];
+        for(int i=y; i<y+n; i++) {
+            for(int j=x; j<x+n; j++) {
+                if(color != arr[i][j]) {
+                    check = false;
+                    break;
+                }
+            }
+            if(!check) break;
+        }
+
+        if(check) {
+            result[color+1]++;
+        } else {
+            int cut = n/3;
+            for(int i=y; i<y+n; i+=cut) {
+                for(int j=x; j<x+n; j+=cut) {
+                    recur(i, j, cut);
+                }
+            }
+        }
+    }
+}
