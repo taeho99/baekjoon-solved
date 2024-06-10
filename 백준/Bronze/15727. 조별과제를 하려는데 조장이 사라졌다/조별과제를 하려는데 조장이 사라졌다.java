26aecd1df70df9536ba460f23e7f982b26f266a8
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-06-10 21:32:27 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 204 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+        System.out.print((n+4)/5);
+    }
+}
