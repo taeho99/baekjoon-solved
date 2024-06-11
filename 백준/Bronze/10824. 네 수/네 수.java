734b0d2b882ebb4bd1b5bdfc769f624bc2169f01
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-06-11 21:43:10 +0900

    [Bronze III] Title: 네 수, Time: 200 ms, Memory: 17688 KB -BaekjoonHub

--- 백준/Bronze/10824. 네 수/네 수.java
+++ 백준/Bronze/10824. 네 수/네 수.java
@@ -1,12 +1,1 @@
-import java.io.IOException;
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int[] arr = new int[4];
-        for (int i=0; i<4; i++)
-            arr[i] = sc.nextInt();
-        System.out.print(Long.parseLong(""+arr[0]+arr[1])+Long.parseLong(""+arr[2]+arr[3]));
-    }
-}
+interface Main{static void main(String[]a){java.util.Scanner s=new java.util.Scanner(System.in);System.out.print(Long.parseLong(s.next()+s.next())+Long.parseLong(s.next()+s.next()));}}
