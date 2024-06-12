9c68304f03456a800a7a47a21658554f90b53844
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-06-12 23:27:12 +0900

    [Bronze III] Title: 웰컴 키트, Time: 232 ms, Memory: 18484 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
@@ -1,20 +1,13 @@
-import java.util.Scanner;
-
-public class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] size = new int[6];
-        for(int i=0; i<6; i++) {
-            size[i] = sc.nextInt();
-        }
-        int t = sc.nextInt(), p = sc.nextInt();
-
-        int result = 0;
-        for(int i=0; i<6; i++) {
-            result += (size[i]+t-1)/t;
-        }
-        System.out.println(result);
-        System.out.println(n/p + " " + n%p);
+import java.util.*;
+class Main{
+    public static void main(String[]a){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        List<Integer> list=new ArrayList<>();
+        for (int i=0;i<6;i++)
+            list.add(sc.nextInt());
+        int t=sc.nextInt(),p=sc.nextInt();
+        System.out.println(list.stream().mapToInt(i -> (i+t-1)/t).sum());
+        System.out.println(n/p+" "+n%p);
     }
 }
