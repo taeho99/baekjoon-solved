23441e5d66f4e8f188e928bc932bfaa83b262550
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-06-13 22:20:49 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 14172 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -3,14 +3,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    static final int MOD = 1234567891;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         String s = br.readLine();
         long sum = 0;
-        for(int i=0; i<n; i++) {
-            sum += (s.charAt(i) - 'a' + 1)*(long)Math.pow(31, i);
+        for(int i = 0; i<n; i++) {
+            long r = 1;
+            for(int j=0; j<i; j++) {
+                r *= 31;
+                r %= MOD;
+            }
+            sum += ((s.charAt(i) - 'a' + 1)% MOD * r% MOD)% MOD;
         }
-        System.out.print(sum%1234567891);
+        System.out.print(sum% MOD);
     }
 }
