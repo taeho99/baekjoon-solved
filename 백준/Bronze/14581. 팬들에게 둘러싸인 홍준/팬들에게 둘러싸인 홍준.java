f03605b68cd946ec0fc49836f80db7fbd4088021
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-06-23 21:28:28 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 244 ms, Memory: 18732 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String s = ":fan:";
+        System.out.println(s+s+s);
+        System.out.println(s+":"+new java.util.Scanner(System.in).next()+":"+s);
+        System.out.println(s+s+s);
+    }
+}
