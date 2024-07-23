76015b017a74612d62bbeb477fa94d2313bd6261
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-07-23 14:57:01 +0900

    [Silver I] Title: 숨바꼭질, Time: 164 ms, Memory: 23576 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,48 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        int k = input.nextInt();
 
-        boolean[] visited = new boolean[100001];
-        Queue<Pair> queue = new LinkedList<>();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		
+		boolean[] visited = new boolean[100001];
+		Queue<int[]> queue = new LinkedList<>();
+	
+		queue.add(new int[] {n, 0});
+		visited[n] = true;
+		
+		while(!queue.isEmpty()) {
+			int[] poll = queue.poll();
+			
+			if(poll[0] == k) {
+				System.out.println(poll[1]);
+				break;
+			}
+			
+			int[] next = {poll[0] - 1, poll[0] + 1, poll[0]*2};
+			
+			for(int i=0; i<3; i++) {
+				if(next[i] >= 0 && next[i] <= 100000 && !visited[next[i]]) {
+					queue.add(new int[] {next[i], poll[1] + 1});
+					visited[next[i]] = true;
+				}
+			}
+		}
+		
+	}
 
-        queue.add(new Pair(n, 0));
-        visited[n] = true;
-
-        while(!queue.isEmpty()) {
-            Pair poll = queue.poll();
-            int x = poll.point;
-            int day = poll.day;
-            if (x == k) {
-                System.out.print(poll.day);
-                return;
-            }
-            if (x+1 >= 0 && x+1 <= 100000 && !visited[x+1]) {
-                visited[x+1] = true;
-                queue.add(new Pair(x+1, day+1));
-            }
-            if (x-1 >= 0 && x-1 <= 100000 && !visited[x-1]) {
-                visited[x-1] = true;
-                queue.add(new Pair(x-1, day+1));
-            }
-            if (x*2 >= 0 && x*2 <= 100000 && !visited[x*2]) {
-                visited[x*2] = true;
-                queue.add(new Pair(x*2, day+1));
-            }
-        }
-    }
-    static class Pair {
-        int point;
-        int day;
-
-        public Pair(int point, int day) {
-            this.point = point;
-            this.day = day;
-        }
-    }
 }
