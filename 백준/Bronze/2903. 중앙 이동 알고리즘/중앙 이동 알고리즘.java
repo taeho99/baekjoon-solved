718f2768017eab9369a25029cbc553b5b9592b6a
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-08-05 23:01:09 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 172 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println((int)Math.pow(Math.pow(2, new java.util.Scanner(System.in).nextInt()) +1, 2));
+    }
+}
