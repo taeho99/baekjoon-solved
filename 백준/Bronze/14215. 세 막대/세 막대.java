7ad6cffd5612faf3d2b1566d6ea66d72ae62eee6
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-08-06 23:41:26 +0900

    [Bronze III] Title: 세 막대, Time: 212 ms, Memory: 17772 KB -BaekjoonHub

---
+++ 백준/Bronze/14215. 세 막대/세 막대.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] s = new int[3];
+        s[0] = sc.nextInt(); s[1] = sc.nextInt(); s[2] = sc.nextInt();
+        Arrays.sort(s);
+        System.out.println(s[0]+s[1] > s[2] ? s[0]+s[1]+s[2] : (s[0]+s[1])*2 - 1);
+    }
+}
