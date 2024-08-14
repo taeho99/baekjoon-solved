06c8668348b229d1a9b5be288bc23c269e0d8007
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-08-14 20:35:51 +0900

    [Silver II] Title: DNA 비밀번호, Time: 172 ms, Memory: 22368 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -1,33 +1,65 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+/**
+ * BOJ.12891 DNA 비밀번호 
+ *
+ * 1. 전체 문자열 길이(sLen), 부분 문자열 길이(pLen), {'A', 'C', 'G', 'T'}를 포함하는 전체 문자열을 입력으로 받는다.
+ * 2. 각 문자가 등장해야 하는 최소 개수를 입력받는다.
+ * 3-1. 가장 첫번째 부분 문자열의 DNA 등장 횟수를 nowFreq에 기록한다.
+ * 3-2. 첫번째 부분 문자열이 최소 조건을 만족하는지 확인한다. 조건에 부합하면 result++ 한다.
+ * 4-1. 슬라이딩 윈도우 방식으로 직전 문자열의 "가장 왼쪽 문자의 빈도를 -1", "가장 오른쪽 문자의 빈도를 +1" 한다.
+ * 4-2. 다음 부분 문자열이 최소 조건을 만족하는지 확인한다. 조건에 부합하면 result++ 한다.
+ * 5. 결과값을 출력한다.
+ */
 public class Main {
-    static int[] require;
-    static int[] now;
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int s = input.nextInt();
-        int p = input.nextInt();
-        char[] dna = input.next().toCharArray();
-        require = new int[4];
-        now = new int[20];
-        for(int i=0; i<4; i++) {
-            require[i] = input.nextInt();
+    static char[] dna = {'A', 'C', 'G', 'T'};
+    static int[] minFreq, nowFreq;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // 1. 전체 문자열 길이(sLen), 부분 문자열 길이(pLen), {'A', 'C', 'G', 'T'}를 포함하는 전체 문자열을 입력으로 받는다.
+        int sLen = Integer.parseInt(st.nextToken());
+        int pLen = Integer.parseInt(st.nextToken());
+        char[] sStr = br.readLine().toCharArray();
+
+        minFreq = new int[26]; // 최소 DNA 등장 개수
+        nowFreq = new int[26]; // 입력된 DNA 등장 개수
+
+        // 2. 각 문자가 등장해야 하는 최소 개수를 입력받는다.
+        st = new StringTokenizer(br.readLine());
+        for(int idx=0; idx<4; idx++) {
+            minFreq[dna[idx] - 'A'] = Integer.parseInt(st.nextToken());
         }
 
-        int result = 0;
-        for(int i = 0; i < p; i++) {
-            now[dna[i] - 'A']++;
+        int result = 0; // 결과값
+
+        // 3-1. 가장 첫번째 부분 문자열의 DNA 등장 횟수를 nowFreq에 기록한다.
+        for(int pIdx=0; pIdx<pLen; pIdx++) {
+            nowFreq[sStr[pIdx] - 'A']++;
         }
-        if (check()) result++;
-        for(int i = p; i < s; i++) {
-            now[dna[i - p] - 'A']--;
-            now[dna[i] - 'A']++;
-            if (check()) result++;
+        // 3-2. 첫번째 부분 문자열이 최소 조건을 만족하는지 확인한다. 조건에 부합하면 result++ 한다.
+        if(isDNA()) result++;
+
+        // 4-1. 슬라이딩 윈도우 방식으로 직전 문자열의 "가장 왼쪽 문자의 빈도를 -1", "가장 오른쪽 문자의 빈도를 +1" 한다.
+        for(int pIdx=pLen; pIdx<sLen; pIdx++) {
+            nowFreq[sStr[pIdx-pLen] - 'A']--;
+            nowFreq[sStr[pIdx] - 'A']++;
+            // 4-2. 다음 부분 문자열이 최소 조건을 만족하는지 확인한다. 조건에 부합하면 result++ 한다.
+            if(isDNA()) result++;
         }
+
+        // 5. 결과값을 출력한다.
         System.out.println(result);
     }
 
-    static boolean check() {
-        return now[0] >= require[0] && now[2] >= require[1] && now[6] >= require[2] && now[19] >= require[3];
+    // 3-2. 해당 부분 문자열의 nowFreq 배열이 완성되었으면, 최소 개수의 조건에 부합하는지 확인한다.
+    private static boolean isDNA() {
+        return nowFreq[dna[0]-'A'] >= minFreq[dna[0]-'A'] && nowFreq[dna[1]-'A'] >= minFreq[dna[1]-'A']
+                && nowFreq[dna[2]-'A'] >= minFreq[dna[2]-'A'] && nowFreq[dna[3]-'A'] >= minFreq[dna[3]-'A'];
     }
+
 }
