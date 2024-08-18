b3cf8bed7d6347961f66527a89db499227687247
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-08-18 22:45:51 +0900

    [Gold IV] Title: 연구소, Time: 296 ms, Memory: 124012 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,93 +1,112 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static final int[] dy = {0, 0, -1, 1};
-    static final int[] dx = {-1, 1, 0, 0};
-    static int[][] arr, copyArr;
-    static int maxSafeZone = Integer.MIN_VALUE;
-    static int n, m;
+    static int rowSize, colSize, ELEMENT_COUNT, SELECT_COUNT, maxSafeArea;
+    static ArrayList<Point> virusList, safeAreaList;
+    static Point[] selectSafeAreaList;
+    static int[][] map;
+    static int[] dRow = {-1, 1, 0, 0};
+    static int[] dCol = {0, 0, -1, 1};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        arr = new int[n][m];
-        for(int i=0; i<n; i++) {
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        rowSize = Integer.parseInt(st.nextToken());
+        colSize = Integer.parseInt(st.nextToken());
+        ELEMENT_COUNT = 0;
+        SELECT_COUNT = 3;
+
+        virusList = new ArrayList<>();
+        safeAreaList = new ArrayList<>();
+        selectSafeAreaList = new Point[SELECT_COUNT];
+        map = new int[rowSize][colSize];
+
+        for(int row=0; row<rowSize; row++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=0; j<m; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+            for(int col=0; col<colSize; col++) {
+                map[row][col] = Integer.parseInt(st.nextToken());
+                if(map[row][col] == 0) {
+                    ELEMENT_COUNT++;
+                    safeAreaList.add(new Point(row, col));
+                } else if (map[row][col] == 2) {
+                    virusList.add(new Point(row, col));
+                }
             }
         }
-        dfs(0);
-        System.out.print(maxSafeZone);
+
+        combination(0, 0);
+        System.out.println(maxSafeArea);
     }
 
-    private static void dfs(int depth) {
-        if(depth == 3) {
-            bfs();
+    private static void combination(int elementIdx, int selectIdx) {
+        if(selectIdx == SELECT_COUNT) {
+//            System.out.println(Arrays.toString(selectSafeAreaList));
+            for (Point safeArea : selectSafeAreaList)
+                map[safeArea.row][safeArea.col] = 1;
+            maxSafeArea = Math.max(maxSafeArea, ELEMENT_COUNT - 3 - bfs());
+            for (Point safeArea : selectSafeAreaList)
+                map[safeArea.row][safeArea.col] = 0;
+            return;
+        }
+        if(elementIdx == ELEMENT_COUNT) {
             return;
         }
 
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                if(arr[i][j] == 0) {
-                    arr[i][j] = 1;
-                    dfs(depth+1);
-                    arr[i][j] = 0;
-                }
-            }
-        }
+        selectSafeAreaList[selectIdx] = safeAreaList.get(elementIdx);
+        combination(elementIdx + 1, selectIdx + 1);
+
+        selectSafeAreaList[selectIdx] = null;
+        combination(elementIdx + 1, selectIdx);
     }
 
-    private static void bfs() {
-        Queue<Node> queue = new LinkedList<>();
-        copyArr = new int[n][m];
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                copyArr[i][j] = arr[i][j];
-                if(arr[i][j] == 2) {
-                    queue.add(new Node(i, j));
-                }
-            }
+    private static int bfs() {
+        Queue<Point> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[rowSize][colSize];
+        int virusCnt = 0;
+        for (Point virus : virusList) {
+            queue.add(virus);
+            visited[virus.row][virus.col] = true;
         }
 
         while(!queue.isEmpty()) {
-            Node poll = queue.poll();
-            for(int i=0; i<4; i++) {
-                int ny = poll.y + dy[i];
-                int nx = poll.x + dx[i];
-                if(ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
-                if(copyArr[ny][nx] == 0) {
-                    copyArr[ny][nx] = 2;
-                    queue.add(new Node(ny, nx));
+            Point poll = queue.poll();
+            virusCnt++;
+
+            for(int dir=0; dir<4; dir++) {
+                int nextRow = poll.row + dRow[dir];
+                int nextCol = poll.col + dCol[dir];
+
+                if(nextRow < 0 || nextRow >= rowSize || nextCol < 0 || nextCol >= colSize) continue;
+
+                if(!visited[nextRow][nextCol] && map[nextRow][nextCol] == 0) {
+                    queue.add(new Point(nextRow, nextCol));
+                    visited[nextRow][nextCol] = true;
                 }
             }
         }
-        calcSafeZone();
+
+        return virusCnt - virusList.size();
     }
 
-    private static void calcSafeZone() {
-        int size = 0;
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                if(copyArr[i][j] == 0)
-                    size++;
-            }
+    static class Point {
+        int row, col;
+
+        public Point(int row, int col) {
+            this.row = row;
+            this.col = col;
         }
-        maxSafeZone = Math.max(size, maxSafeZone);
-    }
 
-    static class Node {
-        int y, x;
-
-        public Node(int y, int x) {
-            this.y = y;
-            this.x = x;
+        @Override
+        public String toString() {
+            return "{" +
+                    "row=" + row +
+                    ", col=" + col +
+                    '}';
         }
     }
 }
