fa06e2d8ee32b679c16e3db7c56933ac5709d6f7
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-08-21 19:26:50 +0900

    [D1] Title: 대각선 출력하기, Time: 108 ms, Memory: 17,416 KB -BaekjoonHub

---
+++ SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.print("#++++\n" +
+                "+#+++\n" +
+                "++#++\n" +
+                "+++#+\n" +
+                "++++#");
+    }
+}
