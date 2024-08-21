c89227aff3e43f127f5998bec861658b4687a050
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-08-21 15:13:42 +0900

    [D4] Title: 추억의 2048게임, Time: 165 ms, Memory: 28,788 KB -BaekjoonHub

--- SWEA/D4/6109. 추억의 2048게임/추억의 2048게임.java
+++ SWEA/D4/6109. 추억의 2048게임/추억의 2048게임.java
@@ -3,6 +3,21 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
  
+/**
+ * 	SWEA.6109 추억의2048게임
+ * 
+ *  1. 맵의 크기와 방향, 맵의 데이터를 입력받는다.
+ *  2. 미는 방향에 따라 4가지 경우로 분류한다. up을 제외한 경우는 배열을 회전한 후 up 한 후 다시 원래 상태로 돌린다.
+ *  	2-1. up: 원본 배열을 회전시키지 않고 up 한다.
+ *  	2-2. down: 원본 배열을 180도 회전 후 up한 뒤 다시 180도 회전시킨다.
+ *  	2-3. left: 원본 배열을 90도 회전시킨 후 up한 뒤 다시 270도 회전시킨다.
+ *  	2-4. right: 원본 배열을 270도 회전시킨 후 up한 뒤 다시 90도 회전시킨다.
+ *  3. up 방향으로 숫자들을 모두 합친다음에 up 방향으로 민다.
+ *  	3-1. 현재 row기준으로 값이 0이 아닌 nextRow를 찾는다.
+ *  	3-2. row 값과 nextRow 값이 같으면 합칠 수 있다. row에 합친 후 nextRow를 0으로 바꿔준다.
+ *  	3-3. up 방향으로 밀어준다. 0인 칸은 밀지 않고 0이 아닌 경우 위에서부터 차례대로 값을 넣어준다.
+ *  4. 완성된 맵을 출력한다.
+ */
 public class Solution {
     static int size;
     static int[][] map, answer;
@@ -14,6 +29,7 @@ public class Solution {
         for(int t=1; t<=testCase; t++) {
             sb.append('#').append(t).append('\n');
  
+            // 1. 맵의 크기와 방향, 맵의 데이터를 입력받는다.
             st = new StringTokenizer(br.readLine());
             size = Integer.parseInt(st.nextToken());
             map = new int[size][size];
@@ -27,42 +43,51 @@ public class Solution {
                 }
             }
  
+            // 2. 미는 방향에 따라 4가지 경우로 분류한다. up을 제외한 경우는 배열을 회전한 후 up 한 후 다시 원래 상태로 돌린다.
             switch(cmd) {
+            	// 2-1. up: 원본 배열을 회전시키지 않고 up 한다.
                 case "up":
                     up();
                     break;
+                // 2-2. down: 원본 배열을 180도 회전 후 up한 뒤 다시 180도 회전시킨다.
                 case "down":
                 	rotate180();
                 	up();
                 	rotate180();
                     break;
+                // 2-3. left: 원본 배열을 90도 회전시킨 후 up한 뒤 다시 270도 회전시킨다.
                 case "left":
                 	rotate90();
                 	up();
                 	rotate270();
                     break;
+                // 2-4. right: 원본 배열을 270도 회전시킨 후 up한 뒤 다시 90도 회전시킨다.
                 case "right":
                 	rotate270();
                 	up();
                 	rotate90();
                     break;
             }
+            // 4. 완성된 맵을 출력한다.
         	printArr();
         }
         System.out.print(sb);
     }
  
+    // 3. up 방향으로 숫자들을 모두 합친다음에 up 방향으로 민다.
     private static void up() {
         for(int col=0; col<size; col++) {
             for(int row=0; row<size-1; row++) {
                 if(map[row][col] == 0) continue;
                 int nextRow = row+1;
  
+                // 3-1. 현재 row기준으로 값이 0이 아닌 nextRow를 찾는다.
                 while(map[nextRow][col] == 0) {
                     if(nextRow == size-1) break;
                     nextRow++;
                 }
  
+                // 3-2. row 값과 nextRow 값이 같으면 합칠 수 있다. row에 합친 후 nextRow를 0으로 바꿔준다.
                 if(map[row][col] == map[nextRow][col]) {
                     map[row][col] += map[nextRow][col];
                     map[nextRow][col] = 0;
@@ -71,6 +96,7 @@ public class Solution {
             }
         }
  
+        // 3-3. up 방향으로 밀어준다. 0인 칸은 밀지 않고 0이 아닌 경우 위에서부터 차례대로 값을 넣어준다.
         for(int col=0; col<size; col++) {
             int nextRow = 0;
             for(int row=0; row<size; row++) {
