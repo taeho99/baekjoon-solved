762dba35bbff7007d996df784fd1ab3c1a7284e5
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-08-21 13:07:11 +0900

    [D5] Title: 현주가 좋아하는 제곱근 놀이, Time: 161 ms, Memory: 31,268 KB -BaekjoonHub

--- SWEA/D5/6782. 현주가 좋아하는 제곱근 놀이/현주가 좋아하는 제곱근 놀이.java
+++ SWEA/D5/6782. 현주가 좋아하는 제곱근 놀이/현주가 좋아하는 제곱근 놀이.java
@@ -1,39 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 
+/**
+ * 1. 정수 num을 입력받는다.
+ * 2. num이 2가 될 때까지 반복한다.
+ * 		2-1. 만약 num이 완전제곱수이면 num = sqrt(num) 해주고 cnt를 1 증가시킨다.
+ * 		2-2. 완전제곱수가 아니면 완전제곱수를 만들어주고 그 수만큼 cnt도 증가시킨다.
+ * 3. 수행 횟수인 cnt를 출력한다.
+ */
 public class Solution {
-	// 100,000,000,000
-	static Map<Long, Long> map;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		init();
 		int T = Integer.parseInt(br.readLine());
 		for(int tc=1; tc<=T; tc++) {
+			// 1. 정수 num을 입력받는다.
 			long num = Long.parseLong(br.readLine());
 			int cnt = 0;
+			// 2. num이 2가 될 때까지 반복한다.
 			while(num != 2) {
-				if(map.containsKey(num)) {
-					num = map.get(num);
+				// 2-1. 만약 num이 완전제곱수이면 num = sqrt(num) 해주고 cnt를 1 증가시킨다.
+				if((long)(Math.pow((long)Math.sqrt(num), 2)) == num) {
+					num = (long)Math.sqrt(num);
 					cnt++;
-				} else {
+				} 
+				// 2-2. 완전제곱수가 아니면 완전제곱수를 만들어주고 그 수만큼 cnt도 증가시킨다.
+				else {
 					long sqrt = (long)Math.sqrt(num);
 					long tmp = (sqrt+1)*(sqrt+1) - num;
 					num += tmp;
 					cnt += tmp;
 				}
 			}
+			// 3. 수행 횟수인 cnt를 출력한다.
 			sb.append('#').append(tc).append(' ').append(cnt).append('\n');
 		}
 		System.out.print(sb);
-	}
-	static void init() {
-		map = new HashMap<>();
-		for(long idx=1; idx<=1_000_000; idx++) {
-			map.put(idx*idx, idx);
-		}
 	}
 }
