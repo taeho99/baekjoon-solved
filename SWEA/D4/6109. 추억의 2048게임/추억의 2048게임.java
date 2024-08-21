fda4f334e2445565757da7c73c0245a720310429
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-08-21 14:49:40 +0900

    [D4] Title: 추억의 2048게임, Time: 154 ms, Memory: 29,576 KB -BaekjoonHub

--- SWEA/D4/6109. 추억의 2048게임/추억의 2048게임.java
+++ SWEA/D4/6109. 추억의 2048게임/추억의 2048게임.java
@@ -2,67 +2,67 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+ 
 public class Solution {
     static int size;
     static int[][] map, answer;
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
         int testCase = Integer.parseInt(br.readLine());
         for(int t=1; t<=testCase; t++) {
             sb.append('#').append(t).append('\n');
-
+ 
             st = new StringTokenizer(br.readLine());
             size = Integer.parseInt(st.nextToken());
             map = new int[size][size];
             answer = new int[size][size];
             String cmd = st.nextToken();
-
+ 
             for(int row=0; row<size; row++) {
                 st = new StringTokenizer(br.readLine());
                 for(int col=0; col<size; col++) {
                     map[row][col] = Integer.parseInt(st.nextToken());
                 }
             }
-
+ 
             switch(cmd) {
                 case "up":
                     up();
                     break;
                 case "down":
-                    down();
+                	rotate180();
+                	up();
+                	rotate180();
                     break;
                 case "left":
-                    left();
+                	rotate90();
+                	up();
+                	rotate270();
                     break;
                 case "right":
-                    right();
+                	rotate270();
+                	up();
+                	rotate90();
                     break;
             }
-
-            for(int row=0; row<size; row++) {
-                for(int col=0; col<size; col++) {
-                    sb.append(answer[row][col]).append(' ');
-                }
-                sb.append('\n');
-            }
+        	printArr();
         }
         System.out.print(sb);
     }
-
+ 
     private static void up() {
         for(int col=0; col<size; col++) {
             for(int row=0; row<size-1; row++) {
                 if(map[row][col] == 0) continue;
                 int nextRow = row+1;
-
+ 
                 while(map[nextRow][col] == 0) {
                     if(nextRow == size-1) break;
                     nextRow++;
                 }
-
+ 
                 if(map[row][col] == map[nextRow][col]) {
                     map[row][col] += map[nextRow][col];
                     map[nextRow][col] = 0;
@@ -70,7 +70,7 @@ public class Solution {
                 }
             }
         }
-
+ 
         for(int col=0; col<size; col++) {
             int nextRow = 0;
             for(int row=0; row<size; row++) {
@@ -79,92 +79,39 @@ public class Solution {
                 }
             }
         }
+        map = answer;
     }
-
-    private static void down() {
-        for(int col=0; col<size; col++) {
-            for(int row=size-1; row>0; row--) {
-                if(map[row][col] == 0) continue;
-                int nextRow = row-1;
-
-                while(map[nextRow][col] == 0) {
-                    if(nextRow == 0) break;
-                    nextRow--;
-                }
-
-                if(map[row][col] == map[nextRow][col]) {
-                    map[row][col] += map[nextRow][col];
-                    map[nextRow][col] = 0;
-                    row = nextRow;
-                }
-            }
-        }
-
-        for(int col=0; col<size; col++) {
-            int nextRow = size-1;
-            for(int row=size-1; row>=0; row--) {
-                if(map[row][col] != 0) {
-                    answer[nextRow--][col] = map[row][col];
-                }
-            }
-        }
+    
+    private static void rotate90() {
+    	int[][] tmpArr = new int[size][size];
+    	for(int row=0; row<size; row++){
+    		for(int col=0; col<size; col++) tmpArr[col][size-1-row] = map[row][col];
+    	}
+    	map = tmpArr;
     }
-
-    private static void left() {
-        for(int row=0; row<size; row++) {
-            for(int col=0; col<size-1; col++) {
-                if(map[row][col] == 0) continue;
-                int nextCol = col+1;
-
-                while(map[row][nextCol] == 0) {
-                    if(nextCol == size-1) break;
-                    nextCol++;
-                }
-
-                if(map[row][col] == map[row][nextCol]) {
-                    map[row][col] += map[row][nextCol];
-                    map[row][nextCol] = 0;
-                    col = nextCol;
-                }
-            }
-        }
-
-        for(int row=0; row<size; row++) {
-            int nextCol = 0;
+    
+    private static void rotate180() {
+    	int[][] tmpArr = new int[size][size];
+    	for(int row=0; row<size; row++){
+    		for(int col=0; col<size; col++) tmpArr[size-1-row][size-1-col] = map[row][col];
+    	}
+    	map = tmpArr;
+    }
+    
+    private static void rotate270() {
+    	int[][] tmpArr = new int[size][size];
+    	for(int row=0; row<size; row++){
+    		for(int col=0; col<size; col++) tmpArr[size-1-col][row] = map[row][col];
+    	}
+    	map = tmpArr;
+    }
+    
+    private static void printArr() {
+    	for(int row=0; row<size; row++) {
             for(int col=0; col<size; col++) {
-                if(map[row][col] != 0) {
-                    answer[row][nextCol++] = map[row][col];
-                }
+                sb.append(map[row][col]).append(' ');
             }
-        }
-    }
-
-    private static void right() {
-        for(int row=0; row<size; row++) {
-            for(int col=size-1; col>0; col--) {
-                if(map[row][col] == 0) continue;
-                int nextCol = col-1;
-
-                while(map[row][nextCol] == 0) {
-                    if(nextCol == 0) break;
-                    nextCol--;
-                }
-
-                if(map[row][col] == map[row][nextCol]) {
-                    map[row][col] += map[row][nextCol];
-                    map[row][nextCol] = 0;
-                    col = nextCol;
-                }
-            }
-        }
-
-        for(int row=0; row<size; row++) {
-            int nextCol = size-1;
-            for(int col=size-1; col>=0; col--) {
-                if(map[row][col] != 0) {
-                    answer[row][nextCol--] = map[row][col];
-                }
-            }
+            sb.append('\n');
         }
     }
 }
