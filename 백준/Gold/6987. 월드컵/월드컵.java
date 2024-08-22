0d13f230feef6d0b36952e8de667464a6e725b35
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-08-22 20:30:11 +0900

    [Gold IV] Title: 월드컵, Time: 64 ms, Memory: 11668 KB -BaekjoonHub

--- 백준/Gold/6987. 월드컵/월드컵.java
+++ 백준/Gold/6987. 월드컵/월드컵.java
@@ -6,14 +6,14 @@ import java.util.StringTokenizer;
 public class Main {
     // 대진표
     static char[][] table = {
-            {'A','O','B'},{'A','O','C'},{'A','O','D'},
-            {'A','O','E'},{'A','O','F'},{'B','O','C'},
-            {'B','O','D'},{'B','O','E'},{'B','O','F'},
-            {'C','O','D'},{'C','O','E'},{'C','O','F'},
-            {'D','O','E'},{'D','O','F'},{'E','O','F'}
+            {'A','B'},{'A','C'},{'A','D'},
+            {'A','E'},{'A','F'},{'B','C'},
+            {'B','D'},{'B','E'},{'B','F'},
+            {'C','D'},{'C','E'},{'C','F'},
+            {'D','E'},{'D','F'},{'E','F'}
     };
 
-    static int[] inputData;
+    static int[][] inputData;
 
     static int[][] result;
 
@@ -26,9 +26,11 @@ public class Main {
         while(T-- > 0) {
             st = new StringTokenizer(br.readLine());
             result = new int[6][3];
-            inputData = new int[18];
-            for(int idx=0; idx<18; idx++) {
-                inputData[idx] = Integer.parseInt(st.nextToken());
+            inputData = new int[6][3];
+            for(int row=0; row<6; row++) {
+                for(int col=0; col<3; col++) {
+                    inputData[row][col] = Integer.parseInt(st.nextToken());
+                }
             }
 
             sb.append(powerSet(0) ? 1 : 0).append(' ');
@@ -38,43 +40,42 @@ public class Main {
 
     static boolean powerSet(int selectIdx) {
         if(selectIdx == 15) {
-            boolean success = true;
             for(int row=0; row<6; row++) {
                 for(int col=0; col<3; col++) {
-                    if(result[row][col] != inputData[row*3+col])
-                        success = false;
+                    if(result[row][col] != inputData[row][col])
+                        return false;
                 }
             }
-            return success;
+            return true;
         }
 
-        result[table[selectIdx][0] - 'A'][0]++;
-        result[table[selectIdx][2] - 'A'][2]++;
-        if(powerSet(selectIdx+1)) return true;
-        result[table[selectIdx][0] - 'A'][0]--;
-        result[table[selectIdx][2] - 'A'][2]--;
+        if(result[table[selectIdx][0] - 'A'][0] < inputData[table[selectIdx][0] - 'A'][0] &&
+                result[table[selectIdx][1] - 'A'][2] < inputData[table[selectIdx][1] - 'A'][2]) {
+            result[table[selectIdx][0] - 'A'][0]++;
+            result[table[selectIdx][1] - 'A'][2]++;
+            if (powerSet(selectIdx + 1)) return true;
+            result[table[selectIdx][0] - 'A'][0]--;
+            result[table[selectIdx][1] - 'A'][2]--;
+        }
 
 
-        result[table[selectIdx][0] - 'A'][1]++;
-        result[table[selectIdx][2] - 'A'][1]++;
-        if(powerSet(selectIdx+1)) return true;
-        result[table[selectIdx][0] - 'A'][1]--;
-        result[table[selectIdx][2] - 'A'][1]--;
+        if(result[table[selectIdx][0] - 'A'][1] < inputData[table[selectIdx][0] - 'A'][1] &&
+                result[table[selectIdx][1] - 'A'][1] < inputData[table[selectIdx][1] - 'A'][1]) {
+            result[table[selectIdx][0] - 'A'][1]++;
+            result[table[selectIdx][1] - 'A'][1]++;
+            if (powerSet(selectIdx + 1)) return true;
+            result[table[selectIdx][0] - 'A'][1]--;
+            result[table[selectIdx][1] - 'A'][1]--;
+        }
 
-        result[table[selectIdx][0] - 'A'][2]++;
-        result[table[selectIdx][2] - 'A'][0]++;
-        if(powerSet(selectIdx+1)) return true;
-        result[table[selectIdx][0] - 'A'][2]--;
-        result[table[selectIdx][2] - 'A'][0]--;
+        if(result[table[selectIdx][0] - 'A'][2] < inputData[table[selectIdx][0] - 'A'][2] &&
+                result[table[selectIdx][1] - 'A'][0] < inputData[table[selectIdx][1] - 'A'][0]) {
+            result[table[selectIdx][0] - 'A'][2]++;
+            result[table[selectIdx][1] - 'A'][0]++;
+            if (powerSet(selectIdx + 1)) return true;
+            result[table[selectIdx][0] - 'A'][2]--;
+            result[table[selectIdx][1] - 'A'][0]--;
+        }
         return false;
-    }
-
-    private static void print() {
-        for(int row=0; row<6; row++) {
-            for(int col=0; col<3; col++) {
-                System.out.print(result[row][col] + " ");
-            }
-            System.out.println();
-        }
     }
 }
