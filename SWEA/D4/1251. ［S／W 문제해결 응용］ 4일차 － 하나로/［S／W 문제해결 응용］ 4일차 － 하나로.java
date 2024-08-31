dea4ed33bc2d29ab24432a16500938fb7c4ed6df
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-08-31 16:00:09 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 251 ms, Memory: 49,124 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -5,31 +5,33 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
- *  SWEA.1251 하나로
+ *  SWEA.1251 하나로_프림
  *
  *  1. 섬의 개수와 각 섬의 좌표, 세율을 입력받는다.
  *      1-1. 각 섬의 좌표는 정점 배열에 저장한다.
- *  2. 정점 배열에서 연결 가능한 모든 간선을 구한다. (간선의 개수: v(v-1)/2)
- *      2-1. 두 정점 사이의 거리를 구하고 환경 부담금을 구해 간선의 비용으로 넣는다.
- *  3. 크루스칼 알고리즘을 위해 간선들을 비용 기준 오름차순으로 정렬한다.
- *  4. 각 정점을 최소 단위 서로소 집합으로 만든다.
- *  5. 간선의 비용이 적은 순부터 간선의 두 정점(start,end)가 아직 연결되지 않았으면 연결한다.
- *      5-1. 연결이 가능하면 현재 간선의 비용을 비용의 총합에 더한다.
- *      5-2. 선택된 간선이 V-1개면 종료한다. (MST는 간선이 V-1개)
- *  6. 최소 비용을 반올림하여 출력한다.
+ *  2. 정점 배열로 가중치가 있는 무향 그래프 인접 행렬을 만든다.
+ *      2-1. 두 정점 사이의 거리를 구하고 환경 부담금을 구해 간선의 비용을 구한다.
+ *  3. 프림 알고리즘을 위해 minEdge[] 배열을 MAX_VALUE로 초기화한다.
+ *  4. 0번 정점을 트리의 시작 정점으로 지정한다.
+ *  5. 방문하지 않은 정점 중 간선의 비용이 최소인 정점을 구한다.
+ *  6. 그 정점을 선택하여 트리에 넣고 비용의 합에 더한다.
+ *  7. 선택된 정점과 다른 정점들을 비교해서 minEdge[] 값을 갱신한다.
+ *  8. 최소 비용을 반올림하여 출력한다.
  */
 public class Solution {
-    static int vertexCnt, edgeCnt;
-    static int[] parents;
+    static int vertexCnt;
+    static boolean[] visited;
     static double taxRate;
-    static Edge[] edges;
+    static double[] minEdge;
+    static double[][] adjMatrix;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st1, st2;
 
         int T = Integer.parseInt(br.readLine());
-        for(int tc=1; tc<=T; tc++) {
+        for (int tc = 1; tc <= T; tc++) {
             sb.append('#').append(tc).append(' ');
 
             // 1. 섬의 개수와 각 섬의 좌표, 세율을 입력받는다.
@@ -38,80 +40,59 @@ public class Solution {
             int[] verticesY = new int[vertexCnt];
             st1 = new StringTokenizer(br.readLine());
             st2 = new StringTokenizer(br.readLine());
-            for(int idx=0; idx<vertexCnt; idx++) {
+            for (int idx = 0; idx < vertexCnt; idx++) {
                 // 1-1. 각 섬의 좌표는 정점 배열에 저장한다.
                 verticesX[idx] = Integer.parseInt(st1.nextToken());
                 verticesY[idx] = Integer.parseInt(st2.nextToken());
             }
             taxRate = Double.parseDouble(br.readLine());
 
-            // 2. 정점 배열에서 연결 가능한 모든 간선을 구한다. (간선의 개수: v(v-1)/2)
-            edgeCnt = vertexCnt*(vertexCnt-1)/2;
-            edges = new Edge[edgeCnt];
-            int idx = 0;
-            for(int start=0; start<vertexCnt; start++) {
-                for(int end=start+1; end<vertexCnt; end++) {
-                    // 2-1. 두 정점 사이의 거리를 구하고 환경 부담금을 구해 간선의 비용으로 넣는다.
+            // 2. 정점 배열로 가중치가 있는 무향 그래프 인접 행렬을 만든다.
+            adjMatrix = new double[vertexCnt][vertexCnt];
+            for (int start = 0; start < vertexCnt; start++) {
+                for (int end = start + 1; end < vertexCnt; end++) {
+                    // 2-1. 두 정점 사이의 거리를 구하고 환경 부담금을 구해 간선의 비용을 구한다.
                     double distSquare = Math.pow(verticesX[start] - verticesX[end], 2) + Math.pow(verticesY[start] - verticesY[end], 2);
-                    edges[idx++] = new Edge(start, end, taxRate*distSquare);
+                    adjMatrix[start][end] = adjMatrix[end][start] = taxRate * distSquare;
                 }
             }
 
-            // 3. 크루스칼 알고리즘을 위해 간선들을 비용 기준 오름차순으로 정렬한다.
-            Arrays.sort(edges);
-            // 4. 각 정점을 최소 단위 서로소 집합으로 만든다.
-            makeSet();
+            // 3. 프림 알고리즘을 위해 minEdge[] 배열을 MAX_VALUE로 초기화한다.
+            minEdge = new double[vertexCnt];
+            visited = new boolean[vertexCnt];
+            Arrays.fill(minEdge, Double.MAX_VALUE);
 
-            int selectEdgeCnt = 0;
+            // 4. 0번 정점을 트리의 시작 정점으로 지정한다.
+            minEdge[0] = 0.0;
             double minimumCost = 0;
-            for (Edge edge : edges) {
-                // 5. 간선의 비용이 적은 순부터 간선의 두 정점(start,end)가 아직 연결되지 않았으면 연결한다.
-                if(union(edge.start, edge.end)) {
-                    // 5-1. 연결이 가능하면 현재 간선의 비용을 비용의 총합에 더한다.
-                    minimumCost += edge.weight;
-                    // 5-2. 선택된 간선이 V-1개면 종료한다. (MST는 간선이 V-1개)
-                    if(++selectEdgeCnt == vertexCnt-1) break;
+            for (int idx = 0; idx < vertexCnt; idx++) {
+                double min = Double.MAX_VALUE;
+                int minVertex = -1;
+
+                // 5. 방문하지 않은 정점 중 간선의 비용이 최소인 정점을 구한다.
+                for (int vertex = 0; vertex < vertexCnt; vertex++) {
+                    if (visited[vertex]) continue;
+                    if (minEdge[vertex] < min) {
+                        minVertex = vertex;
+                        min = minEdge[minVertex];
+                    }
+                }
+
+                // 6. 그 정점을 선택하여 트리에 넣고 비용의 합에 더한다.
+                visited[minVertex] = true;
+                minimumCost += min;
+
+                // 7. 선택된 정점과 다른 정점들을 비교해서 minEdge[] 값을 갱신한다.
+                for (int vertex = 0; vertex < vertexCnt; vertex++) {
+                    if (!visited[vertex] && adjMatrix[minVertex][vertex] < minEdge[vertex]) {
+                        minEdge[vertex] = adjMatrix[minVertex][vertex];
+                    }
                 }
             }
 
-            // 6. 최소 비용을 반올림하여 출력한다.
+            // 8. 최소 비용을 반올림하여 출력한다.
             sb.append(Math.round(minimumCost)).append('\n');
         }
         System.out.print(sb);
-    }
-
-    static void makeSet() {
-        parents = new int[vertexCnt];
-        Arrays.fill(parents, -1);
-    }
-
-    static int findSet(int a) {
-        if(parents[a] < 0) return a;
-        return parents[a] = findSet(parents[a]);
-    }
-
-    static boolean union(int a, int b) {
-        int aRoot = findSet(a);
-        int bRoot = findSet(b);
-        if(aRoot == bRoot) return false;
-        parents[aRoot] += parents[bRoot];
-        parents[bRoot] = aRoot;
-        return true;
-    }
-
-    static class Edge implements Comparable<Edge> {
-        int start, end;
-        double weight;
-
-        public Edge(int start, int end, double weight) {
-            this.start = start;
-            this.end = end;
-            this.weight = weight;
-        }
-
-        @Override
-        public int compareTo(Edge e) {
-            return Double.compare(this.weight, e.weight);
-        }
     }
 }
