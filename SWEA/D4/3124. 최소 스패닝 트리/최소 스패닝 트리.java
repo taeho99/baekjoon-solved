db679afc60e9a5fa4273ead271f1a43b752b5356
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-09-01 01:24:50 +0900

    [D4] Title: 최소 스패닝 트리, Time: 3,341 ms, Memory: 116,460 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,28 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 /**
- *  SWEA.3124 최소스패닝트리_크루스칼
+ *  SWEA.3124 최소스패닝트리_프림
  *
  *  1. 정점의 개수와 간선의 개수를 입력받는다.
- *  2. 인접행렬 정보를 입력받는다.
- *  3. 프림 알고리즘을 위해 minEdge[] 배열을 MAX_VALUE로 초기화한다.
- *  4. 0번 정점을 트리의 시작 정점으로 지정한다.
- *  5. 방문하지 않은 정점 중 간선의 비용이 최소인 정점을 구한다.
- *  6. 그 정점을 선택하여 트리에 넣고 비용의 합에 더한다.
- *  7. 선택된 정점과 다른 정점들을 비교해서 minEdge[] 값을 갱신한다.
+ *  2. 인접리스트 정보를 입력받는다.
+ *  3. 정점이 트리에 속해있는지 여부를 체크하는 배열 초기화
+ *  4. PQ 초기화 -> 비용 기준 오름차순 정렬 (비용이 작은 정점부터 poll 됨)
+ *      4-1. 0번 정점부터 트리에 넣어 인접 정점들을 확인한다.
+ *  5. PQ에서 꺼내면, 비용이 가장 작은 정점이 꺼내짐
+ *      5-1. 만약, 이미 트리에 속한 정점이면 continue
+ *  6. 트리에 속하지 않은 정점이면, 트리에 넣고 비용의 합을 갱신
+ *  7. 현재 정점의 인접 정점들을 모두 PQ에 삽입한다.
  *  8. 최소 비용을 출력한다.
  */
 public class Solution {
-    static int vertexCnt, edgeCnt, selectCnt;
+    static int vertexCnt, edgeCnt;
     static Node[] adjList;
-    static int[] minEdge;
     static boolean[] visited;
     static long minimumCost;
+    static PriorityQueue<Node> pq;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -37,7 +38,7 @@ public class Solution {
             vertexCnt = Integer.parseInt(st.nextToken()); // 0, 1, ... , (vertexCnt-1)
             edgeCnt = Integer.parseInt(st.nextToken());
 
-            // 2. 인접행렬 정보를 입력받는다.
+            // 2. 인접리스트 정보를 입력받는다.
             adjList = new Node[vertexCnt];
             while(edgeCnt-- > 0) {
                 st = new StringTokenizer(br.readLine());
@@ -48,38 +49,37 @@ public class Solution {
                 adjList[to] = new Node(from, weight, adjList[to]);
             }
 
-            // 3. 프림 알고리즘을 위해 minEdge[] 배열을 MAX_VALUE로 초기화한다.
-            minEdge = new int[vertexCnt];
+            // 3. 정점이 트리에 속해있는지 여부를 체크하는 배열 초기화
             visited = new boolean[vertexCnt];
-            Arrays.fill(minEdge, Integer.MAX_VALUE);
-            // 4. 0번 정점을 트리의 시작 정점으로 지정한다.
-            minEdge[0] = 0;
-
             minimumCost = 0;
-            PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.weight, o2.weight));
+
+            // 4. PQ 초기화 -> 비용 기준 오름차순 정렬 (비용이 작은 정점부터 poll 됨)
+            pq = new PriorityQueue<>();
+            // 4-1. 0번 정점부터 트리에 넣어 인접 정점들을 확인한다.
             pq.add(new Node(0, 0, null));
             while(!pq.isEmpty()) {
-                // 5. 방문하지 않은 정점 중 간선의 비용이 최소인 정점을 구한다.
+                // 5. PQ에서 꺼내면, 비용이 가장 작은 정점이 꺼내짐
                 Node minVertex = pq.poll();
+                // 5-1. 만약, 이미 트리에 속한 정점이면 continue
                 if(visited[minVertex.to]) continue;
 
-                // 6. 그 정점을 선택하여 트리에 넣고 비용의 합에 더한다.
+                // 6. 트리에 속하지 않은 정점이면, 트리에 넣고 비용의 합을 갱신
                 visited[minVertex.to] = true;
                 minimumCost += minVertex.weight;
 
-                // 7. 선택된 정점과 다른 정점들을 비교해서 minEdge[] 값을 갱신한다.
+                // 7. 현재 정점의 인접 정점들을 모두 PQ에 삽입한다.
                 for(Node temp = adjList[minVertex.to]; temp != null; temp = temp.next) {
                     pq.add(temp);
                 }
             }
 
-            // 6. 최소 비용을 출력한다.
+            // 8. 최소 비용을 출력한다.
             sb.append(minimumCost).append('\n');
         }
         System.out.print(sb);
     }
 
-    static class Node {
+    static class Node implements Comparable<Node> {
         int to, weight;
         Node next;
 
@@ -87,6 +87,12 @@ public class Solution {
             this.to = to;
             this.weight = weight;
             this.next = next;
+        }
+
+        // 간선의 비용을 기준으로 오름차순 정렬한다.
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(this.weight, o.weight);
         }
     }
 }
