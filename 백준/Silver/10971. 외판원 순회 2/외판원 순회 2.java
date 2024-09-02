046cf70010d8e4b0b29af45d6c0d5cdd029884ab
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-09-02 11:52:42 +0900

    [Silver II] Title: 외판원 순회 2, Time: 84 ms, Memory: 12144 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -4,43 +4,49 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, start, result = Integer.MAX_VALUE;
-    static int[][] map;
-    static boolean[] visited;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        map = new int[n][n];
-
-        for(int i=0; i<n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j=0; j<n; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        for(int i=0; i<n; i++) {
-            visited = new boolean[n];
-            visited[i] = true;
-            start = i;
-            dfs(i, 0, 0);
-        }
-        System.out.println(result);
-    }
-
-    private static void dfs(int now, int sum, int depth) {
-        if(depth == n - 1) {
-            sum += map[now][start];
-            if(map[now][start] != 0)
-                result = Math.min(sum, result);
-            return;
-        }
-        for(int i=0; i<n; i++) {
-            if(!visited[i] && map[now][i] != 0) {
-                visited[i] = true;
-                dfs(i, sum + map[now][i], depth + 1);
-                visited[i] = false;
-            }
-        }
-    }
+	static int size, start, minResult = Integer.MAX_VALUE;
+	static int[][] adjMatrix;
+	static boolean[] visited;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		size = Integer.parseInt(br.readLine());
+		adjMatrix = new int[size][size];
+		
+		for(int row=0; row<size; row++) {
+			st = new StringTokenizer(br.readLine());
+			for(int col=0; col<size; col++) {
+				adjMatrix[row][col] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for(start=0; start<size; start++) {
+			visited = new boolean[size];
+			visited[start] = true;
+			permutation(1, start, 0);
+		}
+		
+		System.out.println(minResult);
+	}
+	private static void permutation(int selectIdx, int prev, int cost) {
+		if(cost >= minResult) {
+			return;
+		}
+		
+		if(selectIdx == size) {
+			if(adjMatrix[prev][start] == 0) return;
+			cost += adjMatrix[prev][start];
+			minResult = Math.min(minResult, cost);
+			return;
+		}
+		
+		for(int next=0; next<size; next++) {
+			if(visited[next] || adjMatrix[prev][next] == 0) continue;
+			
+			visited[next] = true;
+			permutation(selectIdx + 1, next, cost + adjMatrix[prev][next]);
+			visited[next] = false;
+		}
+	}
 }
