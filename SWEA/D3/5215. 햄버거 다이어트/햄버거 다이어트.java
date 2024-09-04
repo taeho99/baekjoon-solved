9f62844db00193c59786d729d7ded9f45849ad04
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-09-04 15:36:49 +0900

    [D3] Title: 햄버거 다이어트, Time: 154 ms, Memory: 29,760 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,113 +1,71 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
- * SWEA.5215 햄버거다이어트_NextPermutation
+ *	SWEA.5215 햄버거다이어트_DP 
  *
- * 1. 햄버거 재료의 점수와 칼로리 정보를 입력받는다.
- * 2. 가능한 재료의 조합을 1개~ELEMENT_COUNT개 모두 확인한다.
- *      2-1. selectList[]에는 선택되는 재료를 [0, 0, 1, 1, 0] 과같은 형식으로 저장된다.
- *      2-2. NextPermutation을 사용하기 위해 미리 오름차순으로 선택되는 재료를 저장한다.
- * 3. 재료를 조합 후 제한 칼로리를 초과하지 않는 재료의 점수의 합을 구한다.
- *      3-1. 현재 selectList[idx]가 선택된 재료라면 점수의 합과 칼로리의 합을 구한다.
- *      3-2. 칼로리의 합이 제한 칼로리를 넘으면 중단한다.
- *      3-3. 모든 점수의 합이 칼로리를 넘지 않는 경우 maxScore를 갱신한다.
- * 4. 최대 조합 가능한 재료의 점수의 합을 출력한다.
+ *	1. 재료 개수와 최대 칼로리를 입력받는다.
+ *	2. 각 재료의 점수와 칼로리를 입력받는다.
+ *	3. 재료를 모두 순회 (각 재료를 선택)
+ *		3-1. 칼로리 제한을 1부터 최대 칼로리까지 순회
+ *			3-1-1. 현재 칼로리가 제한 칼로리를 이하인 경우
+ *				3-1-1-1. 현재(row번) 재료를 넣지 않는 경우 점수와 재료를 넣은 점수 중 큰 값을 dp배열에 넣기
+ *			3-1-2. 현재 칼로리가 제한 칼로리를 넘는 경우
+ *				3-1-2-1. 직전(row-1번) 재료의 최대 점수를 그냥 넣어주기
+ *	4. 최대 점수는 우측 하단 마지막 인덱스이므로 출력
  */
 public class Solution {
-    static int ELEMENT_COUNT, SELECT_COUNT, MAX_CALORIE, maxScore;
-    static int[] selectList, calorieList, scoreList;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-        int T = Integer.parseInt(br.readLine());
-
-        for(int tc=1; tc<=T; tc++) {
-            sb.append('#').append(tc).append(' ');
-            st = new StringTokenizer(br.readLine());
-            ELEMENT_COUNT = Integer.parseInt(st.nextToken());
-            MAX_CALORIE = Integer.parseInt(st.nextToken());
-
-            scoreList = new int[ELEMENT_COUNT];
-            calorieList = new int[ELEMENT_COUNT];
-
-            // 1. 햄버거 재료의 점수와 칼로리 정보를 입력받는다.
-            for(int elementIdx=0; elementIdx<ELEMENT_COUNT; elementIdx++) {
-                st = new StringTokenizer(br.readLine());
-                scoreList[elementIdx] = Integer.parseInt(st.nextToken());
-                calorieList[elementIdx] = Integer.parseInt(st.nextToken());
-            }
-
-            maxScore = 0; // 최고 점수를 저장하는 변수 선언
-            // 2. 가능한 재료의 조합을 1개~ELEMENT_COUNT개 모두 확인한다.
-            for(SELECT_COUNT=1; SELECT_COUNT<=ELEMENT_COUNT; SELECT_COUNT++) {
-                // 2-1. selectList[]에는 선택되는 재료를 [0, 0, 1, 1, 0] 과같은 형식으로 저장된다.
-                selectList = new int[ELEMENT_COUNT];
-                // 2-2. NextPermutation을 사용하기 위해 미리 오름차순으로 선택되는 재료를 저장한다.
-                for(int selectIdx=ELEMENT_COUNT-1; selectIdx>=ELEMENT_COUNT-SELECT_COUNT; selectIdx--) {
-                    selectList[selectIdx] = 1;
-                }
-
-                do {
-                    // 3. 재료를 조합 후 제한 칼로리를 초과하지 않는 재료의 점수의 합을 구한다.
-                    calcScore();
-                } while(np(selectList));
-            }
-
-            // 4. 최대 조합 가능한 재료의 점수의 합을 출력한다.
-            sb.append(maxScore).append('\n');
-        }
-        System.out.print(sb);
-    }
-
-    static void calcScore() {
-        int sumScore = 0;
-        int sumCalorie = 0;
-        for(int selectIdx=0; selectIdx<ELEMENT_COUNT; selectIdx++) {
-            // 3-1. 현재 selectList[idx]가 선택된 재료라면 점수의 합과 칼로리의 합을 구한다.
-            if(selectList[selectIdx] == 1) {
-                sumScore += scoreList[selectIdx];
-                sumCalorie += calorieList[selectIdx];
-            }
-            // 3-2. 칼로리의 합이 제한 칼로리를 넘으면 중단한다.
-            if(sumCalorie > MAX_CALORIE) {
-                return;
-            }
-        }
-        // 3-3. 모든 점수의 합이 칼로리를 넘지 않는 경우 maxScore를 갱신한다.
-        maxScore = Math.max(maxScore, sumScore);
-    }
-
-    static boolean np(int[] selectList) {
-        int selectListSize = selectList.length;
-        // step1) 꼭대기(i) 찾기
-        int i = selectListSize-1;
-        while(i>0 && selectList[i-1] >= selectList[i]) i--;
-        if(i == 0) return false; // 낭떠러지. 다음 순열 없음
-
-        // step2) 꼭대기 앞 교환위치에 교환할 값(i-1 위치 값보다 큰 값 중 가장 작은 값) 자리 찾기
-        int j = selectListSize-1;
-        while(selectList[i-1] >= selectList[j]) j--;
-
-        // step3) 두 위치의 수 교환
-        swap(selectList, i-1, j);
-
-        // step4) 꼭대기부터 맨 뒤까지 오름차순의 형태로 만들기
-        int k = selectListSize-1;
-        while(i < k) {
-            swap(selectList, i++, k--);
-        }
-        return true;
-    }
-
-    static void swap(int[] list, int a, int b) {
-        int temp = list[a];
-        list[a] = list[b];
-        list[b] = temp;
-    }
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	static int T;
+	static int ingredientCnt, maxCalorie;
+	static int[] scores, calories;
+	static int[][] dp;
+	public static void main(String[] args) throws IOException {
+		T = Integer.parseInt(br.readLine());
+		
+		for(int tc=1; tc<=T; tc++) {
+			sb.append('#').append(tc).append(' ');
+			
+			// 1. 재료 개수와 최대 칼로리를 입력받는다.
+			st = new StringTokenizer(br.readLine());
+			ingredientCnt = Integer.parseInt(st.nextToken());
+			maxCalorie = Integer.parseInt(st.nextToken());
+			
+			// 2. 각 재료의 점수와 칼로리를 입력받는다.
+			scores = new int[ingredientCnt+1];
+			calories = new int[ingredientCnt+1];
+			for(int idx=1; idx<=ingredientCnt; idx++) {
+				st = new StringTokenizer(br.readLine());
+				scores[idx] = Integer.parseInt(st.nextToken());
+				calories[idx] = Integer.parseInt(st.nextToken());
+			}
+			
+			dp = new int[ingredientCnt+1][maxCalorie+1];
+			// 3. 재료를 모두 순회 (각 재료를 선택)
+			for(int row=1; row<=ingredientCnt; row++) {
+				// 3-1. 칼로리 제한을 1부터 최대 칼로리까지 순회
+				for(int col=1; col<=maxCalorie; col++) {
+					// 3-1-1. 현재 칼로리가 제한 칼로리를 이하인 경우
+					if(calories[row] <= col) {
+						// 3-1-1-1. 현재(row번) 재료를 넣지 않는 경우 점수와 재료를 넣은 점수 중 큰 값을 dp배열에 넣기
+						dp[row][col] = Math.max(dp[row-1][col], dp[row-1][col-calories[row]] + scores[row]);
+					} 
+					// 3-1-2. 현재 칼로리가 제한 칼로리를 넘는 경우
+					else {
+						// 3-1-2-1. 직전(row-1번) 재료의 최대 점수를 그냥 넣어주기
+						dp[row][col] = dp[row-1][col];
+					}
+				}
+			}
+			
+			// 4. 최대 점수는 우측 하단 마지막 인덱스이므로 출력
+			sb.append(dp[ingredientCnt][maxCalorie]).append('\n');
+		}
+		System.out.print(sb);
+	}
 
 }
