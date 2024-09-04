b63b90495cb1ae45d56ea7ba3b045d7c7f7a56ad
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-09-04 16:13:34 +0900

    [Silver III] Title: 1로 만들기, Time: 92 ms, Memory: 15544 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,19 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+/**
+ * 
+ * @author SSAFY
+ *	역으로 1부터 연산해가며 num 까지 만들기
+ */
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		int n = input.nextInt();
-		int dp[] = new int[n+1];
-		dp[0] = dp[1] = 0;
-		for(int i=2; i<=n; i++) {
-			dp[i] = dp[i-1] + 1;
-			if(i%2 == 0)
-				dp[i] = Math.min(dp[i],  dp[i/2] + 1);
-			if(i%3 == 0)
-				dp[i] = Math.min(dp[i],  dp[i/3] + 1);
+	public static void main(String[] args) throws IOException {
+		int num = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
+		int[] dp = new int[num+1];
+		for(int idx=2; idx<=num; idx++) {
+			dp[idx] = dp[idx-1] + 1;
+			if(idx%2 == 0) 
+				dp[idx] = dp[idx] < (dp[idx/2]+1) ? dp[idx] : (dp[idx/2]+1);
+			if(idx%3 == 0) 
+				dp[idx] = dp[idx] < (dp[idx/3]+1) ? dp[idx] : (dp[idx/3]+1);
 		}
-		System.out.println(dp[n]);
+		System.out.println(dp[num]);
 	}
+
 }
