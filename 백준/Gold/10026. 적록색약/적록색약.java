152b936afaec40e176999a77a72ec0e457699d8b
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-09-04 19:17:08 +0900

    [Gold V] Title: 적록색약, Time: 88 ms, Memory: 12976 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -4,64 +4,77 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ *  BOJ.10026 적록색약
+ *
+ *  1. 맵의 크기와 정보 입력받기
+ *  2. 모든 지점에서 BFS 돌면서 같은 구역 갯수 구하기
+ *  3. 적록색약은 R==G로 느끼므로 G를 모두 R로 바꾸기
+ *  4. 적록색약인 사람이 모든 지점에서 BFS 돌면서 같은 구역 갯수 구하기
+ *  5. 출력하기
+ */
 public class Main {
-    static int[] dy = {-1, 1, 0, 0};
-    static int[] dx = {0, 0, -1, 1};
+    static int[] dRow = {-1, 1, 0, 0};
+    static int[] dCol = {0, 0, -1, 1};
+    static int size;
     static char[][] map;
-    static int n;
+    static boolean[][] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        map = new char[n][n];
-
-        for(int i=0; i<n; i++) {
+        // 1. 맵의 크기와 정보 입력받기
+        size = Integer.parseInt(br.readLine());
+        map = new char[size][size];
+        for(int i=0; i<size; i++) {
             String tmp = br.readLine();
-            for(int j=0; j<n; j++) {
+            for(int j=0; j<size; j++) {
                 map[i][j] = tmp.charAt(j);
             }
         }
 
-        boolean[][] visited = new boolean[n][n];
-        int answer_a = bfs(visited);
+        // 2. 모든 지점에서 BFS 돌면서 같은 구역 갯수 구하기
+        sb.append(bfs()).append(' ');
 
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
+        // 3. 적록색약은 R==G로 느끼므로 G를 모두 R로 바꾸기
+        for(int i=0; i<size; i++) {
+            for(int j=0; j<size; j++) {
                 if (map[i][j] == 'G')
                     map[i][j] = 'R';
             }
         }
-        visited = new boolean[n][n];
-        int answer_b = bfs(visited);
 
-        System.out.print(answer_a + " ");
-        System.out.print(answer_b);
+        // 4. 적록색약인 사람이 모든 지점에서 BFS 돌면서 같은 구역 갯수 구하기
+        sb.append(bfs());
+
+        // 5. 출력하기
+        System.out.print(sb);
     }
 
-    private static int bfs(boolean[][] visited) {
+    private static int bfs() {
+        visited = new boolean[size][size];
         int answer = 0;
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                if (!visited[i][j]) {
-                    Queue<Point> queue = new LinkedList<>();
-                    queue.add(new Point(i, j, map[i][j]));
-                    visited[i][j] = true;
-                    answer++;
+        for(int row=0; row<size; row++) {
+            for(int col=0; col<size; col++) {
+                if(visited[row][col]) continue;
+                answer++;
 
-                    while(!queue.isEmpty()) {
-                        Point poll = queue.poll();
-                        int y = poll.y;
-                        int x = poll.x;
-                        char color = poll.color;
+                Queue<Point> queue = new LinkedList<>();
+                queue.add(new Point(row, col, map[row][col]));
+                visited[row][col] = true;
 
-                        for(int k=0; k<4; k++) {
-                            int ny = y + dy[k];
-                            int nx = x + dx[k];
-                            if (ny<0 || ny>=n || nx<0 || nx>=n) continue;
-                            if (map[ny][nx] == color && !visited[ny][nx]) {
-                                queue.add(new Point(ny, nx, map[ny][nx]));
-                                visited[ny][nx] = true;
-                            }
+                while(!queue.isEmpty()) {
+                    Point poll = queue.poll();
+
+                    for(int dir=0; dir<4; dir++) {
+                        int nRow = poll.row + dRow[dir];
+                        int nCol = poll.col + dCol[dir];
+
+                        if (nRow<0 || nRow>=size || nCol<0 || nCol>=size) continue;
+
+                        if (map[nRow][nCol] == poll.color && !visited[nRow][nCol]) {
+                            queue.add(new Point(nRow, nCol, map[nRow][nCol]));
+                            visited[nRow][nCol] = true;
                         }
                     }
                 }
@@ -71,11 +84,11 @@ public class Main {
     }
 
     static class Point {
-        int y, x;
+        int row, col;
         char color;
-        public Point(int y, int x, char color) {
-            this.y = y;
-            this.x = x;
+        public Point(int row, int col, char color) {
+            this.row = row;
+            this.col = col;
             this.color = color;
         }
     }
