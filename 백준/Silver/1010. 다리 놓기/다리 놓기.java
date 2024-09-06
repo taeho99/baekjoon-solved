548c6d26178b8ea8b76d4f9cead76a3be01890fe
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-09-06 16:46:31 +0900

    [Silver V] Title: 다리 놓기, Time: 68 ms, Memory: 11752 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -3,25 +3,45 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * 	BOJ.1010 다리놓기
+ * 
+ * 	0. r: 강 서쪽 사이트 수 / n: 강 동쪽 사이트 수 -> n개 중 r개 조합으로 뽑으면 됨
+ * 	0. dp[n][r] : nCr 값 메모이제이션 
+ * 	1. n과 r 입력으로 받기
+ * 	2. nCr 구하고 출력하기
+ * 		2-1. 메모이제이션 한 값이 있으면 그 값 반환하기
+ * 		2-2. n==r 또는 r==0 이면 무조건 1이므로 메모이제이션 하고 반환하기
+ * 		2-3. 두 경우 다 아니면 직접 계산해서 메모이제이션 하고 반환하기
+ */
 public class Main {
-    static int[][] dp = new int[30][30];
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-        int t = Integer.parseInt(br.readLine());
-        while(t-- > 0) {
-            st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
-            sb.append(combi(m, n)).append('\n');
-        }
-        System.out.print(sb);
-    }
-
-    static int combi(int n, int r) {
-        if(dp[n][r] > 0) return dp[n][r];
-        if(n == r || r == 0) return dp[n][r] = 1;
-        return dp[n][r] = combi(n-1, r-1) + combi(n-1, r);
-    }
+	// 0. r: 강 서쪽 사이트 수 / n: 강 동쪽 사이트 수 -> n개 중 r개 조합으로 뽑으면 됨
+	static int n, r;
+	// 0. dp[n][r] : nCr 값 메모이제이션 
+	static int[][] dp = new int[30][30];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		while(T-- > 0) {
+			// 1. n과 r 입력으로 받기
+			st = new StringTokenizer(br.readLine());
+			r = Integer.parseInt(st.nextToken());
+			n = Integer.parseInt(st.nextToken());
+			
+			// 2. nCr 구하고 출력하기
+			sb.append(combination(n, r)).append('\n');
+		}
+		System.out.print(sb);
+	}
+	static int combination(int n, int r) {
+		// 2-1. 메모이제이션 한 값이 있으면 그 값 반환하기
+		if(dp[n][r] > 0) return dp[n][r];
+		// 2-2. n==r 또는 r==0 이면 무조건 1이므로 메모이제이션 하고 반환하기
+		if(n==r || r==0) return dp[n][r] = 1;
+		// 2-3. 두 경우 다 아니면 직접 계산해서 메모이제이션 하고 반환하기
+		return dp[n][r] = combination(n-1, r) + combination(n-1, r-1);
+	}
 }
