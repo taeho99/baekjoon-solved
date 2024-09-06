cc3d31d4bdc5350452d317e678b040e9f0e329ca
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-09-06 16:14:17 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보물상자 비밀번호, Time: 120 ms, Memory: 19,412 KB -BaekjoonHub

--- SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.java
+++ SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.java
@@ -12,9 +12,14 @@ import java.util.StringTokenizer;
 /**
  *	SWEA.5658 보물상자비밀번호
  *
- *	1. 회전 횟수는 N/4번 반복
- *	2. 가능한 수를 세트에 넣기
- *	3. 리스트로 바꾸고 오름차순 정렬하기
+ *	1. 숫자 개수와 k번째 인덱스를 입력받는다.
+ *	2. 숫자를 문자열로 입력받는다.
+ *		2-1. 숫자를 회전시키는 대신 숫자를 2배 길이로 늘려준다.
+ *	3. 한 변의 숫자 개수만큼 회전시켜준다.
+ *		3-1. 변 4개 모두 확인
+ *			3-1-1. 한 변에 있는 수를 10진수로 변환 후 중복 제거를 위해 set에 넣기
+ *	4. set를 list로 변환 후 내림차순 정렬하기
+ *	5. k번째 인덱스에 있는 값 출력하기
  */
 public class Solution {
 	static int size, kIdx;
@@ -31,26 +36,33 @@ public class Solution {
 		for(int tc=1; tc<=T; tc++) {
 			sb.append('#').append(tc).append(' ');
 			
+			// 1. 숫자 개수와 k번째 인덱스를 입력받는다.
 			st = new StringTokenizer(br.readLine());
 			size = Integer.parseInt(st.nextToken());
 			kIdx = Integer.parseInt(st.nextToken())-1; // 인덱스므로 -1
+			
+			// 2. 숫자를 문자열로 입력받는다.
 			nums = br.readLine();
-			
-			
+			// 2-1. 숫자를 회전시키는 대신 숫자를 2배 길이로 늘려준다.
 			nums = nums + nums;
 			
 			set = new HashSet<>();
 			int quater = size/4;
+			// 3. 한 변의 숫자 개수만큼 회전시켜준다.
 			for(int rotate=0; rotate<quater; rotate++) {
+				// 3-1. 변 4개 모두 확인
 				for(int dir=0; dir<4; dir++) {
+					// 3-1-1. 한 변에 있는 수를 10진수로 변환 후 중복 제거를 위해 set에 넣기
 					int num = Integer.parseInt(nums.substring(quater*dir+rotate, quater*(dir+1)+rotate), 16);
 					set.add(num);
 				}
 			}
 			
+			// 4. set를 list로 변환 후 내림차순 정렬하기
 			list = new ArrayList<>(set);
 			Collections.sort(list, Comparator.reverseOrder());
 			
+			// 5. k번째 인덱스에 있는 값 출력하기
 			sb.append(list.get(kIdx)).append('\n');
 		}
 		System.out.print(sb);
