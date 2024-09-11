b15c1f453ba3f79e747a9a26937266edc2539ed2
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-09-11 16:42:47 +0900

    [Gold IV] Title: N-Queen, Time: 5912 ms, Memory: 12008 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,44 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
 
+/**
+ *	SWEA.2806 N-Queen
+ *	
+ */
 public class Main {
-    static int result = 0, n;
-    static boolean[] columnSet, plusSet, minusSet;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        columnSet = new boolean[n];
-        plusSet = new boolean[2*n];
-        minusSet = new boolean[2*n];
-        dfs(0);
-        System.out.print(result);
-    }
-
-    static void dfs(int row) {
-        if(row == n) {
-            result++;
-            return;
-        }
-
-        for(int col=0; col<n; col++) {
-            if(promise(row, col)) {
-                columnSet[col] = true;
-                plusSet[col+row] = true;
-                minusSet[(n-1)-col+row] = true;
-                dfs(row+1);
-                columnSet[col] = false;
-                plusSet[col+row] = false;
-                minusSet[(n-1)-col+row] = false;
-            }
-        }
-    }
-
-    static boolean promise(int row, int col) {
-        if(columnSet[col] || plusSet[col+row] || minusSet[(n-1)-col+row])
-            return false;
-        return true;
-    }
+	static int n, result;
+	static int[] colIdx;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		n = Integer.parseInt(br.readLine());
+		
+		colIdx = new int[n];
+		backtrack(0);
+			
+		System.out.print(result);
+	}
+	
+	private static void backtrack(int rowIdx) {
+		if(!isPromise(rowIdx-1)) return;
+		
+		if(rowIdx == n) {
+			result++;
+			return;
+		}
+		
+	
+		for(int col=0; col<n; col++) {
+			colIdx[rowIdx] = col;
+			backtrack(rowIdx+1);
+		}
+	}
+	
+	private static boolean isPromise(int rowIdx) {
+		for(int row=0; row<rowIdx; row++) {
+			if(colIdx[row] == colIdx[rowIdx] 
+					|| rowIdx-row == Math.abs(colIdx[rowIdx]-colIdx[row])) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
