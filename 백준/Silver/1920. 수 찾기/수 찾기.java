e840c3642bcb3c7ebdddf7d341e9b8e9ad2139bd
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-09-23 15:41:45 +0900

    [Silver IV] Title: 수 찾기, Time: 420 ms, Memory: 55748 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,49 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
-        int n = Integer.parseInt(br.readLine());
-        List<Integer> a = new ArrayList<>();
-        StringTokenizer a_input = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++) {
-            a.add(Integer.parseInt(a_input.nextToken()));
-        }
-
-        Collections.sort(a);
-
-        int m = Integer.parseInt(br.readLine());
-        a_input = new StringTokenizer(br.readLine());
-        for(int i=0; i<m; i++) {
-            boolean flag = false;
-            int findNum = Integer.parseInt(a_input.nextToken());
-            int low = 0;
-            int high = a.size() - 1;
-            while(low <= high) {
-                int mid = (low + high) / 2;
-                if (a.get(mid) == findNum) {
-                    sb.append("1").append("\n");
-                    flag = true;
-                    break;
-                } else if (findNum < a.get(mid)) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
-                }
-            }
-            if (!flag) {
-                sb.append("0").append("\n");
-            }
-        }
-        System.out.println(sb);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		Set<String> set = new HashSet<>();
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		while(n-- > 0) {
+			set.add(st.nextToken());
+		}
+		
+		int m = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		while(m-- > 0) {
+			sb.append(set.contains(st.nextToken()) ? 1 : 0).append('\n');
+		}
+		System.out.print(sb);
+	}
 }
