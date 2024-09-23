d5dc414e259834f1b7a34b133e622f17226f4baf
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-09-23 20:47:59 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1016 ms, Memory: 180856 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,24 +1,52 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int n, arr[];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        HashMap<Integer, Integer> map = new HashMap<>();
 
-        int n = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++) {
-            int key = Integer.parseInt(st.nextToken());
-            map.put(key, map.getOrDefault(key, 0) + 1);
+        for(int idx=0; idx<n; idx++) {
+            arr[idx] = Integer.parseInt(st.nextToken());
         }
+
+        Arrays.sort(arr);
 
         int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<m; i++) {
-            sb.append(map.getOrDefault(Integer.parseInt(st.nextToken()), 0)).append(' ');
+            int key = Integer.parseInt(st.nextToken());
+            sb.append(getUpperIdx(key) - getLowerIdx(key)).append(' ');
         }
         System.out.print(sb);
+    }
+
+    private static int getLowerIdx(int key) {
+        int lo = 0, hi = n;
+        while(lo < hi) {
+            int mid = (lo+hi)/2;
+            if(arr[mid] >= key) hi = mid;
+            else lo = mid+1;
+        }
+        return lo;
+    }
+
+    private static int getUpperIdx(int key) {
+        int lo = 0, hi = n;
+        while(lo < hi) {
+            int mid = (lo+hi)/2;
+            if(arr[mid] > key) hi = mid;
+            else lo = mid+1;
+        }
+        return lo;
     }
 }
