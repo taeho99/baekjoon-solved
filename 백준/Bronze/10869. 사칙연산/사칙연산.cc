3d4355f4d5a7bbeaea37382ea7c18ee78625f0b7
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-11-29 23:48:24 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(void)
+{
+	using namespace std;
+
+	int a, b;
+	cin >> a >> b;
+	cout << a+b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+	cout << a / b << endl;
+	cout << a % b << endl;
+	return 0;
+}
