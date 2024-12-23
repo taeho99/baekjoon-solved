784c15bc17271f633cb4458104311621f8080401
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-12-24 01:37:21 +0900

    [Gold V] Title: 최소비용 구하기, Time: 404 ms, Memory: 51980 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -7,9 +7,9 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
+
     static class Node implements Comparable<Node> {
-        int end;
-        int weight;
+        int end, weight;
 
         public Node(int end, int weight) {
             this.end = end;
@@ -26,48 +26,46 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
+        int vertexSize = Integer.parseInt(br.readLine());
+        int edgeSize = Integer.parseInt(br.readLine());
 
-        ArrayList<ArrayList<Node>> adj = new ArrayList<>();
-        int[] dist = new int[n+1];
-        boolean[] visited = new boolean[n+1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-
-        for(int i=0; i<=n; i++) {
-            adj.add(new ArrayList<>());
+        ArrayList<Node>[] graph = new ArrayList[vertexSize + 1];
+        for(int idx=1; idx<=vertexSize; idx++) {
+            graph[idx] = new ArrayList<>();
         }
 
-        for(int i=0; i<m; i++) {
+        while(edgeSize-- > 0) {
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
 
-            adj.get(start).add(new Node(end, weight));
+            graph[u].add(new Node(v, w));
         }
+
+        int[] best = new int[vertexSize + 1];
+        Arrays.fill(best, Integer.MAX_VALUE);
+        PriorityQueue<Node> pq = new PriorityQueue<>();
 
         st = new StringTokenizer(br.readLine());
         int start = Integer.parseInt(st.nextToken());
         int end = Integer.parseInt(st.nextToken());
 
-        PriorityQueue<Node> queue = new PriorityQueue<>();
-        queue.add(new Node(start, 0));
-        dist[start] = 0;
+        best[start] = 0;
+        pq.add(new Node(start, 0));
 
-        while(!queue.isEmpty()) {
-            Node poll = queue.poll();
-            if(!visited[poll.end]) {
-                visited[poll.end] = true;
+        while(!pq.isEmpty()) {
+            Node cur = pq.poll();
+            if(cur.weight > best[cur.end]) continue;
 
-                for (Node node : adj.get(poll.end)) {
-                    if(!visited[node.end] && dist[node.end] > dist[poll.end] + node.weight) {
-                        dist[node.end] = dist[poll.end] + node.weight;
-                        queue.add(new Node(node.end, dist[node.end]));
-                    }
+            for (Node node : graph[cur.end]) {
+                if(best[node.end] > cur.weight + node.weight) {
+                    best[node.end] = cur.weight + node.weight;
+                    pq.add(new Node(node.end, best[node.end]));
                 }
             }
         }
-        System.out.println(dist[end]);
+
+        System.out.println(best[end]);
     }
 }
