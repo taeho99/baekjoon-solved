cd8abffd4a50a36d676060a95a0c78a90d532362
taeho99 <70526479+taeho99@users.noreply.github.com>
2024-12-29 22:55:37 +0900

    [Bronze III] Title: 별 찍기 - 16, Time: 68 ms, Memory: 11600 KB -BaekjoonHub

---
+++ 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n-i; j++) {
+                sb.append(' ');
+            }
+            for(int j=1; j<=i; j++) {
+                sb.append("* ");
+            }
+            sb.append('\n');
+        }
+        System.out.print(sb);
+    }
+}
