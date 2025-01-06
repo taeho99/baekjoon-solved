28d2f1317cf9f67b02f6393f0d44028fccec23a1
taeho99 <70526479+taeho99@users.noreply.github.com>
2025-01-06 22:45:44 +0900

    [Silver V] Title: 돌 게임 2, Time: 68 ms, Memory: 11516 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
+        System.out.print(n%2==0?"SK":"CY");
+    }
+}
