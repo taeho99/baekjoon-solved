f6e3a8a5658803b5528d25c4cd3d671a5832bdb7
taeho99 <70526479+taeho99@users.noreply.github.com>
2025-02-02 18:45:31 +0900

    [Bronze IV] Title: 세수정렬, Time: 108 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] n = new int[3];
+        for(int i=0; i<3; i++) {
+            n[i] = input.nextInt();
+        }
+        quickSort(n, 0, 2);
+        for(int i=0; i<3; i++) {
+            System.out.print(n[i] + " ");
+        }
+    }
+    public static void quickSort(int[] data, int start, int end) {
+        if(start >= end) {
+            return;
+        }
+        int key = start;
+        int i = start + 1;
+        int j = end;
+        int temp;
+
+        while(i <= j) {
+            while(i <= end && data[i] <= data[key]) {
+                i++;
+            }
+            while(j > start && data[j] >= data[key]) {
+                j--;
+            }
+            if(i > j) {
+                temp = data[j];
+                data[j] = data[key];
+                data[key] = temp;
+            } else {
+                temp = data[j];
+                data[j] = data[i];
+                data[i] = temp;
+            }
+        }
+        quickSort(data, start, j-1);
+        quickSort(data, j+1, end);
+    }
+}
