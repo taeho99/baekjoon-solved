95a89213055ec57fd0629f24f6e19ec2dfc79b0c
taeho99 <70526479+taeho99@users.noreply.github.com>
2025-02-13 22:05:38 +0900

    [Bronze IV] Title: 입실 관리, Time: 68 ms, Memory: 11520 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5524. 입실 관리/입실 관리.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
+        while(t-- > 0) {
+            sb.append(br.readLine().toLowerCase()).append('\n');
+        }
+        System.out.print(sb);
+    }
+}
