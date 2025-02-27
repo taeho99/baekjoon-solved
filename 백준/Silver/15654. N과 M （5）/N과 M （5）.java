cc8703b68d4898a84b4a4fe6cf0aeab0cd10392d
taeho99 <70526479+taeho99@users.noreply.github.com>
2025-02-27 10:56:31 +0900

    [Silver III] Title: N과 M (5), Time: 212 ms, Memory: 31000 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -2,47 +2,51 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] arr, num;
-    static boolean[] visited;
-    static int n, m;
-    static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        arr = new int[m];
-        num = new int[n];
-        visited = new boolean[n];
+	static int elementCnt, selectCnt;
+	static int[] elementList, selectList;
+	static boolean[] visited;
+	static StringBuilder sb = new StringBuilder();
 
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++) {
-            num[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(num);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        dfs(0);
-        System.out.print(sb);
-    }
+		elementCnt = Integer.parseInt(st.nextToken());
+		selectCnt = Integer.parseInt(st.nextToken());
+		visited = new boolean[elementCnt];
+		elementList = new int[elementCnt];
+		selectList = new int[selectCnt];
 
-    private static void dfs(int depth) {
-        if(depth == m) {
-            for (int i : arr) {
-                sb.append(i).append(' ');
-            }
-            sb.append('\n');
-            return;
-        }
-        for(int i=0; i<n; i++) {
-            if(!visited[i]) {
-                visited[i] = true;
-                arr[depth] = num[i];
-                dfs(depth+1);
-                visited[i] = false;
-            }
-        }
-    }
+		st = new StringTokenizer(br.readLine());
+		for(int idx=0; idx<elementCnt; idx++) {
+			elementList[idx] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(elementList);
+
+		permutation(0);
+
+		System.out.print(sb);
+	}
+
+	private static void permutation(int selectIdx) {
+		if (selectIdx == selectCnt) {
+			for (int select : selectList) {
+				sb.append(select).append(' ');
+			}
+			sb.append('\n');
+			return;
+		}
+
+		for(int elementIdx=0; elementIdx<elementCnt; elementIdx++) {
+			if(visited[elementIdx]) continue;
+			visited[elementIdx] = true;
+			selectList[selectIdx] = elementList[elementIdx];
+			permutation(selectIdx + 1);
+			visited[elementIdx] = false;
+		}
+	}
 }
