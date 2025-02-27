3c71ca7284a004a2f3112ede0032744c1d448b9e
taeho99 <70526479+taeho99@users.noreply.github.com>
2025-02-27 16:43:56 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 68 ms, Memory: 12468 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -4,48 +4,57 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int max = Integer.MIN_VALUE;
-    static int min = Integer.MAX_VALUE;
-    static int[] operator = new int[4];
-    static int[] number;
-    static int n;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        number = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++) {
-            number[i] = Integer.parseInt(st.nextToken());
-        }
+	static int max = Integer.MIN_VALUE;
+	static int min = Integer.MAX_VALUE;
+	static int n;
+	static int[] numList, opList;
 
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<4; i++) {
-            operator[i] = Integer.parseInt(st.nextToken());
-        }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		numList = new int[n];
+		opList = new int[4];
 
-        dfs(number[0], 1);
-        System.out.println(max);
-        System.out.println(min);
-    }
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int idx=0; idx<n; idx++) {
+			numList[idx] = Integer.parseInt(st.nextToken());
+		}
 
-    static void dfs(int num, int idx) {
-        if(idx == n) {
-            max = Math.max(max, num);
-            min = Math.min(min, num);
-            return;
-        }
+		st = new StringTokenizer(br.readLine());
+		for(int idx=0; idx<4; idx++) {
+			opList[idx] = Integer.parseInt(st.nextToken());
+		}
 
-        for(int i=0; i<4; i++) {
-            if(operator[i] > 0) {
-                operator[i]--;
-                switch(i) {
-                    case 0: dfs(num + number[idx], idx+1); break;
-                    case 1: dfs(num - number[idx], idx+1); break;
-                    case 2: dfs(num * number[idx], idx+1); break;
-                    case 3: dfs(num / number[idx], idx+1); break;
-                }
-                operator[i]++;
-            }
-        }
-    }
+		calc(numList[0], 0);
+		System.out.println(max);
+		System.out.println(min);
+	}
+
+	private static void calc(int sum, int selectIdx) {
+		if(selectIdx == n - 1) {
+			max = Math.max(max, sum);
+			min = Math.min(min, sum);
+			return;
+		}
+
+		for(int opIdx=0; opIdx<4; opIdx++) {
+			if(opList[opIdx] == 0) continue;
+			opList[opIdx]--;
+			switch(opIdx) {
+				case 0:
+					calc(sum + numList[selectIdx + 1], selectIdx + 1);
+					break;
+				case 1:
+					calc(sum - numList[selectIdx + 1], selectIdx + 1);
+					break;
+				case 2:
+					calc(sum * numList[selectIdx + 1], selectIdx + 1);
+					break;
+				case 3:
+					calc(sum / numList[selectIdx + 1], selectIdx + 1);
+					break;
+			}
+			opList[opIdx]++;
+		}
+	}
 }
