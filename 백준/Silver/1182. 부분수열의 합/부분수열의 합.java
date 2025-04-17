7e11dec3d505d62df2a79f4a56e5aeb8ded235aa
taeho99 <70526479+taeho99@users.noreply.github.com>
2025-04-17 16:59:48 +0900

    [Silver II] Title: 부분수열의 합, Time: 88 ms, Memory: 11488 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -4,34 +4,34 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, s;
-    static int[] arr;
-    static int result = 0;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        s = Integer.parseInt(st.nextToken());
-        arr = new int[n];
+	static int n, s, result = 0;
+	static int[] arr;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
+		n = Integer.parseInt(st.nextToken());
+		s = Integer.parseInt(st.nextToken());
+		arr = new int[n];
 
-        dfs(0, 0);
-        System.out.println(s == 0 ? result - 1 : result);
-    }
+		st = new StringTokenizer(br.readLine());
+		for(int idx=0; idx<n; idx++) {
+			arr[idx] = Integer.parseInt(st.nextToken());
+		}
 
-    private static void dfs(int depth, int sum) {
-        if(depth == n) {
-            if(sum == s) {
-                result++;
-            }
-            return;
-        }
+		dfs(0, 0);
+		System.out.print(s == 0 ? result - 1 : result);
+	}
 
-        dfs(depth+1, sum+arr[depth]);
-        dfs(depth+1, sum);
-    }
+	static void dfs(int sum, int depth) {
+		if (depth == n) {
+			if (sum == s) {
+				result++;
+			}
+			return;
+		}
+
+		dfs(sum + arr[depth], depth + 1);
+		dfs(sum, depth + 1);
+	}
 }
