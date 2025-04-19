ebe65333b7b562b17cd346b1cb584d7ec3ebd802
taeho99 <70526479+taeho99@users.noreply.github.com>
2025-04-19 18:55:59 +0900

    [Silver V] Title: 수들의 합, Time: 68 ms, Memory: 11552 KB -BaekjoonHub

--- 백준/Silver/1789. 수들의 합/수들의 합.java
+++ 백준/Silver/1789. 수들의 합/수들의 합.java
@@ -1,17 +1,19 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner input = new Scanner(System.in);
-        long s = input.nextLong();
-        long sum = 0;
-        int result = 0;
-        for(int i=1; ; i++) {
-            if (sum > s) break;
-            sum += i;
-            result++;
-        }
-        System.out.println(--result);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long s = Long.parseLong(br.readLine());
+
+		long sum = 0;
+		for(int i=1; ; i++) {
+			if(sum > s) {
+				System.out.println(i-2);
+				return;
+			}
+			sum += i;
+		}
+	}
 }
