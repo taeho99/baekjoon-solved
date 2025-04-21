b7f906698e5fae7dbc6febee295e96f4fdf23788
taeho99 <70526479+taeho99@users.noreply.github.com>
2025-04-21 21:34:08 +0900

    [Silver II] Title: 사탕 게임, Time: 144 ms, Memory: 12244 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.java
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.java
@@ -2,25 +2,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * N*N 크기에 색이 다른 사탕
- * 사탕의 색이 다른 인접한 두 칸 고름
- * 두 칸에 들어있는 사탕 교환
- * 같은 색이면서 가장 긴 사탕 행 OR 열 없앰
- */
 public class Main {
     static char[][] map;
     static int size, result = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         size = Integer.parseInt(br.readLine());
+
         map = new char[size][size];
 
         for(int row=0; row<size; row++) {
-            String input = br.readLine();
-            for(int col=0; col<size; col++) {
-                map[row][col] = input.charAt(col);
-            }
+            map[row] = br.readLine().toCharArray();
         }
 
         for(int row=0; row<size; row++) {
@@ -31,45 +23,50 @@ public class Main {
             }
         }
 
-        for(int col=0; col<size; col++) {
-            for(int row=0; row<size-1; row++) {
+        for(int row=0; row<size-1; row++) {
+            for(int col=0; col<size; col++) {
                 swap(row, col, row+1, col);
                 search();
                 swap(row, col, row+1, col);
             }
         }
-        System.out.println(result);
+
+        System.out.print(result);
     }
 
     private static void search() {
         for(int row=0; row<size; row++) {
+            char prev = map[row][0];
             int cnt = 1;
-            for(int col=0; col<size-1; col++) {
-                if(map[row][col] == map[row][col+1]) {
+            for(int col=1; col<size; col++) {
+                if (prev == map[row][col]) {
                     cnt++;
-                    result = Math.max(result, cnt);
+                    result = Math.max(cnt, result);
                 } else {
                     cnt = 1;
                 }
+                prev = map[row][col];
             }
         }
 
         for(int col=0; col<size; col++) {
+            char prev = map[0][col];
             int cnt = 1;
-            for(int row=0; row<size-1; row++) {
-                if(map[row][col] == map[row+1][col]) {
+            for(int row=1; row<size; row++) {
+                if (prev == map[row][col]) {
                     cnt++;
-                    result = Math.max(result, cnt);
+                    result = Math.max(cnt, result);
                 } else {
                     cnt = 1;
                 }
+                prev = map[row][col];
             }
         }
     }
 
     private static void swap(int row1, int col1, int row2, int col2) {
-        char tmp = map[row1][col1];
+        char temp = map[row1][col1];
         map[row1][col1] = map[row2][col2];
-        map[row2][col2] = tmp;
+        map[row2][col2] = temp;
     }
 }
