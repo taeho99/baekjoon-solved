86eabb7ee6e9e4e52d461b3fb04db6c7d42cb980
taeho99 <70526479+taeho99@users.noreply.github.com>
2025-04-25 10:13:41 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 76 ms, Memory: 11808 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -4,55 +4,54 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        char[][] arr = new char[n][m];
-        for(int i=0; i<n; i++) {
-            String tmp = br.readLine();
-            for(int j=0; j<m; j++) {
-                arr[i][j] = tmp.charAt(j);
-            }
-        }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int rowSize = Integer.parseInt(st.nextToken());
+		int colSize = Integer.parseInt(st.nextToken());
 
-        int min = Integer.MAX_VALUE;
-        for(int i=0; i<=n-8; i++) {
-            for(int j=0; j<=m-8; j++) {
-                int sum1 = 0, sum2 = 0;
-                for(int a=0; a<8; a++) {
-                    for(int b=0; b<8; b++) {
-                        if(a%2 == 0) {
-                            if(b%2 == 0) {
-                                if(arr[a+i][b+j] != 'W')
-                                    sum1++;
-                                else
-                                    sum2++;
-                            } else {
-                                if(arr[a+i][b+j] != 'B')
-                                    sum1++;
-                                else
-                                    sum2++;
-                            }
-                        } else {
-                            if(b%2 == 0) {
-                                if(arr[a+i][b+j] != 'B')
-                                    sum1++;
-                                else
-                                    sum2++;
-                            } else {
-                                if(arr[a+i][b+j] != 'W')
-                                    sum1++;
-                                else
-                                    sum2++;
-                            }
-                        }
-                    }
-                }
-                min = Math.min(min, Math.min(sum1, sum2));
-            }
-        }
-        System.out.println(min);
-    }
+		char[][] map = new char[rowSize][colSize];
+		for(int row=0; row<rowSize; row++) {
+			map[row] = br.readLine().toCharArray();
+		}
+
+		int result = Integer.MAX_VALUE;
+		for(int startRow=0; startRow<=rowSize-8; startRow++) {
+			for(int startCol=0; startCol<=colSize-8; startCol++) {
+				int sum1 = 0, sum2 = 0;
+				for(int row=0; row<8; row++) {
+					for(int col=0; col<8; col++) {
+						if (row % 2 == 0) {
+							if (col % 2 == 0) {
+								if (map[row+startRow][col+startCol] != 'W')
+									sum1++;
+								else
+									sum2++;
+							} else {
+								if (map[row+startRow][col+startCol] != 'B')
+									sum1++;
+								else
+									sum2++;
+							}
+						} else {
+							if (col % 2 == 0) {
+								if (map[row+startRow][col+startCol] != 'B')
+									sum1++;
+								else
+									sum2++;
+							} else {
+								if (map[row+startRow][col+startCol] != 'W')
+									sum1++;
+								else
+									sum2++;
+							}
+						}
+					}
+				}
+
+				result = Math.min(result, Math.min(sum1, sum2));
+			}
+		}
+		System.out.println(result);
+	}
 }
