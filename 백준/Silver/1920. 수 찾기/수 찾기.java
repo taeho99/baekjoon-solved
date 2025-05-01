a44ebf8d3ba47b095a4674bf7400092a3f630768
taeho99 <70526479+taeho99@users.noreply.github.com>
2025-05-01 19:43:36 +0900

    [Silver IV] Title: 수 찾기, Time: 404 ms, Memory: 50160 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,26 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		
-		Set<String> set = new HashSet<>();
+
 		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		while(n-- > 0) {
-			set.add(st.nextToken());
+		for(int idx=0; idx<n; idx++) {
+			arr[idx] = Integer.parseInt(st.nextToken());
 		}
-		
+
+		Arrays.sort(arr);
+
 		int m = Integer.parseInt(br.readLine());
 		st = new StringTokenizer(br.readLine());
 		while(m-- > 0) {
-			sb.append(set.contains(st.nextToken()) ? 1 : 0).append('\n');
+			int target = Integer.parseInt(st.nextToken());
+			sb.append(Arrays.binarySearch(arr, target) < 0 ? 0 : 1).append('\n');
 		}
 		System.out.print(sb);
 	}
