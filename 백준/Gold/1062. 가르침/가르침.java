eefb29a88a2659246dae1167c397f80e3a075eaf
taeho99 <70526479+taeho99@users.noreply.github.com>
2025-06-21 22:11:15 +0900

    [Gold IV] Title: 가르침, Time: 444 ms, Memory: 82692 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.java
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -6,71 +6,58 @@ import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int ELEMENT_COUNT, SELECT_COUNT, result;
-    static char[] elementList, selectList;
-    static String[] words;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	static int wordCnt, alphaCnt, resultMax = 0;
+	static String[] words;
+	static HashSet<Character> set = new HashSet<>(Arrays.asList('a', 'n', 't', 'i', 'c'));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int wordsCnt = Integer.parseInt(st.nextToken());
-        words = new String[wordsCnt];
+		wordCnt = Integer.parseInt(st.nextToken());
+		alphaCnt = Integer.parseInt(st.nextToken());
 
-        ELEMENT_COUNT = 26;
-        SELECT_COUNT = Integer.parseInt(st.nextToken());
+		words = new String[wordCnt];
+		for(int idx=0; idx<wordCnt; idx++) {
+			words[idx] = br.readLine();
+		}
 
-        for(int idx=0; idx<wordsCnt; idx++) {
-            words[idx] = br.readLine();
-        }
+		if(alphaCnt < 5) {
+			System.out.println("0");
+			return;
+		}
 
-        if(SELECT_COUNT < 5) {
-            System.out.println("0");
-            return;
-        }
+		combination(5, 0);
+		System.out.println(resultMax);
+	}
 
-        elementList = new char[]
-                {'a','c','i','n','t','b','d','e','f','g','h','j','k','l','m',
-                        'o','p','q','r','s','u','v','w','x','y','z'};
-        selectList = new char[SELECT_COUNT];
-        for(int idx=0; idx<5; idx++) {
-            selectList[idx] = elementList[idx];
-        }
+	private static void combination(int selectIdx, int at) {
+		if (selectIdx == alphaCnt) {
+			int readableCnt = 0;
+			for (String word : words) {
+				if (isReadable(word)) {
+					readableCnt++;
+				}
+			}
+			resultMax = Math.max(resultMax, readableCnt);
+			return;
+		}
 
-        result = 0;
-        combination(5, 5);
-        System.out.println(result);
-    }
+		for(int idx=at; idx<26; idx++) {
+			char alpha = (char)('a' + idx);
+			if (set.contains(alpha)) continue;
 
-    private static void combination(int elementIdx, int selectIdx) {
-//        if(selectList[0] != 'a') return;
-        if(selectIdx == SELECT_COUNT) {
-            HashSet<Character> set = new HashSet<>();
-            for (char ch : selectList) {
-                set.add(ch);
-            }
+			set.add(alpha);
+			combination(selectIdx + 1, idx + 1);
+			set.remove(alpha);
+		}
+	}
 
-            int cnt = 0;
-            for (String word : words) {
-                if(isReadWord(word, set)) cnt++;
-            }
-            result = Math.max(result, cnt);
-            return;
-        }
-        if(elementIdx == ELEMENT_COUNT) {
-            return;
-        }
-
-        selectList[selectIdx] = elementList[elementIdx];
-        combination(elementIdx + 1, selectIdx + 1);
-
-        combination(elementIdx + 1, selectIdx);
-    }
-
-    private static boolean isReadWord(String word, HashSet<Character> set) {
-
-        for (char ch : word.toCharArray()) {
-            if (!set.contains(ch)) return false;
-        }
-        return true;
-    }
+	private static boolean isReadable(String word) {
+		for(int idx=4; idx<word.length() - 4; idx++) {
+			if (!set.contains(word.charAt(idx))) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
