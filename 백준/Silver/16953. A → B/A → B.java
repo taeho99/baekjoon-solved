5566f139c6a333856790f576dd67277160f8a0a3
taeho99 <70526479+taeho99@users.noreply.github.com>
2025-07-02 17:54:32 +0900

    [Silver II] Title: A → B, Time: 184 ms, Memory: 25156 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.java
+++ 백준/Silver/16953. A → B/A → B.java
@@ -1,34 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		long original = Integer.parseInt(st.nextToken());
+		long target = Integer.parseInt(st.nextToken());
 
-        int result = 1;
+		System.out.println(bfs(original, target));
 
-        while(a != b) {
-            if(a > b) {
-                System.out.println(-1);
-                return;
-            }
+	}
 
-            String s = String.valueOf(b);
-            if(b % 2 == 0) {
-                b /= 2;
-            } else if (s.charAt(s.length()-1) == '1') {
-                b = Integer.parseInt(s.substring(0, s.length()-1));
-            } else {
-                System.out.println(-1);
-                return;
-            }
-            result++;
-        }
-        System.out.println(result);
-    }
+	private static long bfs(long original, long target) {
+		HashSet<Long> visited = new HashSet<>();
+		Queue<long[]> queue = new LinkedList<>();
+
+		visited.add(original);
+		queue.add(new long[] {original, 0});
+
+		while(!queue.isEmpty()) {
+			long[] poll = queue.poll();
+
+			if (poll[0] == target) {
+				return poll[1] + 1;
+			}
+
+			long next1 = poll[0] * 2L;
+			long next2 = poll[0] * 10L + 1L;
+			if (!visited.contains(next1) && next1 <= target) {
+				visited.add(next1);
+				queue.add(new long[] {next1, poll[1] + 1});
+			}
+			if (!visited.contains(next2) && next2 <= target) {
+				visited.add(next2);
+				queue.add(new long[] {next2, poll[1] + 1});
+			}
+		}
+		return -1;
+	}
 }
