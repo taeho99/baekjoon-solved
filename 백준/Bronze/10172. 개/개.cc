7a4b868115665fa8c2a7f20c110c8560e1616487
taeho99 <70526479+taeho99@users.noreply.github.com>
2025-07-03 23:32:19 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(void)
+{
+	using namespace std;
+
+	cout << "|\\_/|" << endl;
+	cout << "|q p|   /}" << endl;
+	cout << "( 0 )\"\"\"\\" << endl;
+	cout << "|\"^\"`    |" << endl;
+	cout << "||_/=\\\\__|" << endl;
+	return 0;
+}
