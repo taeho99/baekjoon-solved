f27dbd2b63085c72d20c288dc4123328febe3ce7
taeho99 <70526479+taeho99@users.noreply.github.com>
2025-07-04 21:12:31 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 184 ms, Memory: 64820 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -6,44 +6,46 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int n = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken());
-		
-		int[] visited = new int[100001];
+
+		int original = Integer.parseInt(st.nextToken());
+		int target = Integer.parseInt(st.nextToken());
+
+		int minTime = Integer.MAX_VALUE;
+
 		Queue<int[]> queue = new LinkedList<>();
-	
-		queue.add(new int[] {n, 0});
-		visited[n] = 1;
-		int count = 0, time = 0;
-		
+		int[] visited = new int[100_001];
+
+		queue.add(new int[] {original, 0});
+
+		int cnt = 0;
 		while(!queue.isEmpty()) {
 			int[] poll = queue.poll();
-			
-			if(poll[0] == k) {
-				if(count == 0) {
-					time = poll[1];
+
+			if (poll[0] == target) {
+				if (minTime == Integer.MAX_VALUE) {
+					minTime = poll[1];
 				}
-				if(time == poll[1]) {
-					count++;
+				if (minTime == poll[1]) {
+					cnt++;
 				}
+				continue;
 			}
-			
-			int[] next = {poll[0] - 1, poll[0] + 1, poll[0]*2};
-			
-			for(int i=0; i<3; i++) {
-				if(next[i] >= 0 && next[i] <= 100000 && (visited[next[i]] == 0 || visited[next[i]] == poll[1]+1)) {
-					queue.add(new int[] {next[i], poll[1] + 1});
-					visited[next[i]] = poll[1]+1;
+
+			int[] next = {poll[0] - 1, poll[0] + 1, poll[0] * 2};
+
+			for(int idx=0; idx<3; idx++) {
+				if (next[idx] < 0 || next[idx] > 100_000) continue;
+				if (visited[next[idx]] == 0 || visited[next[idx]] == poll[1] + 1) {
+					queue.add(new int[] {next[idx], poll[1] + 1});
+					visited[next[idx]] = poll[1] + 1;
 				}
 			}
 		}
-		System.out.println(time);
-		System.out.println(count);
-	}
 
+		System.out.println(minTime);
+		System.out.println(cnt);
+	}
 }
