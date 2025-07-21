2302e922ad5e7dfb06409d550c353be9059dea42
taeho99 <70526479+taeho99@users.noreply.github.com>
2025-07-21 19:52:49 +0900

    [Gold V] Title: 퇴사 2, Time: 608 ms, Memory: 302776 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -5,26 +5,23 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] time = new int[n+2];
-        int[] pay = new int[n+2];
-        int[] dp = new int[n+2];
-        for(int i=1; i<=n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            time[i] = Integer.parseInt(st.nextToken());
-            pay[i] = Integer.parseInt(st.nextToken());
-        }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int totalDay = Integer.parseInt(br.readLine());
+		int[] dp = new int[totalDay+2];
 
-        int max = -1;
-        for(int i=1; i<=n+1; i++) {
-            max = Math.max(max, dp[i]);
+		for (int day = 1; day <= totalDay; day++) {
+			st = new StringTokenizer(br.readLine());
+			int t = Integer.parseInt(st.nextToken());
+			int p = Integer.parseInt(st.nextToken());
 
-            int end = i + time[i];
-            if(end <= n+1)
-                dp[end] = Math.max(dp[end], max + pay[i]);
-        }
-        System.out.println(dp[n+1]);
-    }
+			dp[day] = Math.max(dp[day], dp[day - 1]);
+			if (day + t <= totalDay + 1) {
+				dp[day + t] = Math.max(dp[day + t], dp[day] + p);
+			}
+		}
+		dp[totalDay+1] = Math.max(dp[totalDay], dp[totalDay+1]);
+		System.out.println(dp[totalDay+1]);
+	}
 }
