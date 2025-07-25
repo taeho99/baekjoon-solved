c7a81150846f8be3d05f97f83b4c9a669e698af9
taeho99 <70526479+taeho99@users.noreply.github.com>
2025-07-25 14:16:43 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 204 ms, Memory: 40916 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -1,30 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
-import java.util.ArrayList;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        BigInteger count = new BigInteger("2");
-        sb.append(count.pow(n).subtract(new BigInteger("1"))).append('\n');
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		int n = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
+		hanoi(1,3,2,n);
+		System.out.println(sb.length() / 4);
+		System.out.print(sb);
+	}
 
-        if(n <= 20) {
-            hanoi(n, 1, 2, 3);
-        }
-        System.out.print(sb);
-    }
+	private static void hanoi(int start, int end, int temp, int n) {
+		if (n <= 0) {
+			return;
+		}
 
-    static void hanoi(int n, int a, int b, int c) {
-        if(n == 1) {
-            sb.append(a).append(' ').append(c).append('\n');
-        } else {
-            hanoi(n-1, a, c, b);
-            sb.append(a).append(' ').append(c).append('\n');
-            hanoi(n-1, b, a, c);
-        }
-    }
+		hanoi(start, temp, end, n-1);
+		sb.append(start).append(' ').append(end).append('\n');
+		hanoi(temp, end, start, n-1);
+	}
 }
