3bc884256dee03312fe62c8c7c9d2e5ea5edd77d
taeho99 <70526479+taeho99@users.noreply.github.com>
2025-07-26 23:47:17 +0900

    [Bronze III] Title: 최소, 최대, Time: 868 ms, Memory: 128464 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,13 +1,14 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		int n = input.nextInt();
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
 		int[] a = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=0; i<n; i++) {
-			a[i] = input.nextInt();
+			a[i] = Integer.parseInt(st.nextToken());
 		}
 		Arrays.sort(a);
 		System.out.println(a[0] + " " + a[a.length - 1]);
