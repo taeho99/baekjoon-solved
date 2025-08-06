c6d306c477f2cd902660c031bc05dd97266cc7de
taeho99 <70526479+taeho99@users.noreply.github.com>
2025-08-06 14:16:00 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1600 ms, Memory: 481968 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,23 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int t = Integer.parseInt(br.readLine());
-        int[] arr = new int[t];
-        for(int i=0; i<t; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-        Arrays.sort(arr);
-        for (int i : arr) {
-            sb.append(i).append('\n');
-        }
-        System.out.print(sb);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int size = Integer.parseInt(br.readLine());
+
+		int[] freq = new int[10001];
+
+		for(int idx=0; idx<size; idx++) {
+			freq[Integer.parseInt(br.readLine())]++;
+		}
+
+		for(int idx=1; idx<10001; idx++) {
+			for(int j=0; j<freq[idx]; j++) {
+				sb.append(idx).append('\n');
+			}
+		}
+		System.out.print(sb);
+	}
 }
