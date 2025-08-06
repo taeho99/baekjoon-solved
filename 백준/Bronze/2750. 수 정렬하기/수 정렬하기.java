45b126461451a6f3d1b41e25e52bbe5ac8507486
taeho99 <70526479+taeho99@users.noreply.github.com>
2025-08-06 14:29:43 +0900

    [Bronze II] Title: 수 정렬하기, Time: 84 ms, Memory: 12040 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -6,26 +6,31 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		int size = Integer.parseInt(br.readLine());
-		int[] arr = new int[size];
-
-		for(int idx=0; idx<size; idx++) {
+		int[] arr = new int[Integer.parseInt(br.readLine())];
+		for(int idx=0; idx<arr.length; idx++) {
 			arr[idx] = Integer.parseInt(br.readLine());
 		}
-
-		for(int i=size-1; i>=0; i--) {
-			for(int j=0; j<i; j++) {
-				if(arr[j] > arr[j+1]) {
-					int tmp = arr[j];
-					arr[j] = arr[j+1];
-					arr[j+1] = tmp;
-				}
-			}
-		}
-
+		sortBySelectionSort(arr);
 		for (int i : arr) {
 			sb.append(i).append('\n');
 		}
 		System.out.print(sb);
 	}
+
+	private static void sortBySelectionSort(int[] arr) {
+		for(int i=0; i<arr.length-1; i++) {
+			int minIdx = i;
+			for(int j=i+1; j<arr.length; j++) {
+				if(arr[minIdx] > arr[j]) minIdx = j;
+			}
+			swap(arr, i, minIdx);
+		}
+	}
+
+	private static void swap(int[] arr, int i, int j) {
+		int tmp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = tmp;
+	}
+
 }
