be6df3624f41b9a7c854b726509ece1941cb7b26
taeho99 <70526479+taeho99@users.noreply.github.com>
2025-08-06 16:28:19 +0900

    [Bronze II] Title: 수 정렬하기, Time: 76 ms, Memory: 12288 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -10,11 +10,47 @@ public class Main {
 		for(int idx=0; idx<arr.length; idx++) {
 			arr[idx] = Integer.parseInt(br.readLine());
 		}
-		sortBySelectionSort(arr);
+		sortByMergeSort(arr);
 		for (int i : arr) {
 			sb.append(i).append('\n');
 		}
 		System.out.print(sb);
+	}
+
+	private static void sortByMergeSort(int[] arr) {
+		int[] tmp = new int[arr.length];
+		mergeSort(arr, tmp, 0, arr.length-1);
+	}
+
+	private static void mergeSort(int[] arr, int[] tmp, int start, int end) {
+		if (start < end) {
+			int mid = (start + end) / 2;
+			mergeSort(arr, tmp, start, mid);
+			mergeSort(arr, tmp, mid+1, end);
+			merge(arr, tmp, start, mid, end);
+		}
+	}
+
+	private static void merge(int[] arr, int[] tmp, int start, int mid, int end) {
+		for(int i=start; i<=end; i++) {
+			tmp[i] = arr[i];
+		}
+
+		int part1 = start;
+		int part2 = mid+1;
+		int index = start;
+
+		while(part1 <= mid && part2 <= end) {
+			if(tmp[part1] <= tmp[part2]) {
+				arr[index++] = tmp[part1++];
+			} else {
+				arr[index++] = tmp[part2++];
+			}
+		}
+
+		for(int i=0; i<=mid-part1; i++) {
+			arr[index+i] = tmp[part1+i];
+		}
 	}
 
 	private static void sortBySelectionSort(int[] arr) {
