d1e6a5c612a76ef34ff8026b50ee2babdca286c3
taeho99 <70526479+taeho99@users.noreply.github.com>
2025-08-20 21:22:03 +0900

    [Bronze III] Title: 첫 글자를 대문자로, Time: 60 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/4458. 첫 글자를 대문자로/첫 글자를 대문자로.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        while(T-- > 0) {
+            String str = br.readLine();
+            sb.append(Character.toUpperCase(str.charAt(0))).append(str.substring(1)).append('\n');
+        }
+        System.out.print(sb);
+    }
+}
