6a522bd957cdd15ced478b85af3b964ff6f7857d
taeho99 <70526479+taeho99@users.noreply.github.com>
2025-09-03 22:25:16 +0900

    [level 2] Title: 카펫, Time: 7.95 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,16 +1,10 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
-
-        int sum = brown + yellow;
-        for(int width = 3; width <= sum; width++) {
-            if(sum % width == 0) {
-                int height = sum / width;
-                if((width-2) * (height-2) == yellow) {
-                    answer[0] = height;
-                    answer[1] = width;
-                    return answer;
-                }
+        int[] answer = {};
+        for(int height = 3; height<=1250; height++) {
+            for(int width = height; width<=2500; width++) {
+                if((height-2)*(width-2) == yellow && (width+width+height+height-4) == brown)
+                    return new int[] {width, height};
             }
         }
         return answer;
