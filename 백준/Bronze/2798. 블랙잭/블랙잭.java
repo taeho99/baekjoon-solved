33c28ea030dfb819028dbcdee330843f15693756
taeho99 <70526479+taeho99@users.noreply.github.com>
2025-10-08 23:57:51 +0900

    [Bronze II] Title: 블랙잭, Time: 72 ms, Memory: 11624 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,42 +1,33 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static boolean[] visited;
-    static int[] card;
-    static int result = 0, n, m;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        card = new int[n];
-        visited = new boolean[n];
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++) {
-            card[i] = Integer.parseInt(st.nextToken());
-        }
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int elementSize = Integer.parseInt(st.nextToken());
+		int target = Integer.parseInt(st.nextToken());
 
-        dfs(0, 0);
-        System.out.println(result);
-    }
+		int[] elementList = new int[elementSize];
+		st = new StringTokenizer(br.readLine());
+		for(int idx=0; idx<elementSize; idx++) {
+			elementList[idx] = Integer.parseInt(st.nextToken());
+		}
 
-    private static void dfs(int start, int depth) {
-        if(depth == 3) {
-            int sum = 0;
-            for(int i=0; i<n; i++) {
-                if(visited[i]) sum += card[i];
-            }
-            if(sum > result && sum <= m) {
-                result = sum;
-            }
-            return;
-        }
-        for(int i=start; i<n; i++) {
-            visited[i] = true;
-            dfs(i+1, depth+1);
-            visited[i] = false;
-        }
-    }
+		int result = 0;
+		for(int i=0; i<elementSize; i++) {
+			for(int j=i+1; j<elementSize; j++) {
+				for(int k=j+1; k<elementSize; k++) {
+					int sum = elementList[i] + elementList[j] + elementList[k];
+					if(sum <= target) {
+						result = Math.max(result, sum);
+					}
+				}
+			}
+		}
+		System.out.println(result);
+	}
 }
