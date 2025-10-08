0d185c4fdd7f2669944075f22dcca26540164f5b
taeho99 <70526479+taeho99@users.noreply.github.com>
2025-10-09 01:32:41 +0900

    [Silver V] Title: 영화감독 숌, Time: 272 ms, Memory: 112640 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -1,16 +1,22 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        int count = 1;
-        int num = 666;
-        while(count != n) {
-            num++;
-            if(String.valueOf(num).contains("666"))
-                count++;
-        }
-        System.out.println(num);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		int cnt = 0;
+		for(int i=666; i<1_000_000_000; i++) {
+			if(isJongNum(i)) cnt++;
+			if(cnt == num) {
+				System.out.println(i);
+				break;
+			}
+		}
+	}
+
+	private static boolean isJongNum(int num) {
+		return ("" + num).contains("666");
+	}
 }
