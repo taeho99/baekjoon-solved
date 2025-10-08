1cf5e0307ae18e36bfc17c7baf7b0a795d32fb54
taeho99 <70526479+taeho99@users.noreply.github.com>
2025-10-09 01:04:33 +0900

    [level 2] Title: 주차 요금 계산, Time: 19.22 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,61 +1,52 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int[] fees, String[] records) {
-        HashMap<String, Integer> time = new HashMap<>();
-        HashMap<String, Integer> lastIn = new HashMap<>();
-        HashSet<String> isIn = new HashSet<>();
-        HashMap<String, Integer> result = new HashMap<>();
-        for (String record : records) {
-            String[] split = record.split(" ");
-            int hour = Integer.parseInt(split[0].split(":")[0]);
-            int minute = Integer.parseInt(split[0].split(":")[1]);
-            String number = split[1];
-            String inout = split[2];
-
-            int timeToInt = hour*60 + minute;
-
-            System.out.println("[" + inout + "] " + number + ", " + timeToInt);
-
-            if(inout.equals("IN")) {
-                lastIn.put(number, timeToInt);
-                isIn.add(number);
+        HashMap<String, Integer> answer = new HashMap<>();
+        HashMap<String, Integer> inMap = new HashMap<>();
+        HashMap<String, Integer> outMap = new HashMap<>();
+        for(String record : records) {
+            String[] data = record.split(" ");
+            String[] hhmm = data[0].split(":");
+            int time = Integer.parseInt(hhmm[0]) * 60 + Integer.parseInt(hhmm[1]);
+            
+            if (data[2].equals("IN")) {
+                inMap.put(data[1], time);
             } else {
-                int diff = timeToInt - lastIn.get(number);
-                time.put(number, time.getOrDefault(number, 0) + diff);
-                isIn.remove(number);
+                int diffTime = time - inMap.get(data[1]);
+                outMap.put(data[1], outMap.getOrDefault(data[1], 0) + diffTime);
+                inMap.remove(data[1]);
             }
         }
-
-        for (String number : isIn) {
-            int outTime = 23*60 + 59;
-            int diff = outTime - lastIn.get(number);
-            time.put(number, time.getOrDefault(number, 0) + diff);
+        
+        for(Map.Entry<String, Integer> entry : inMap.entrySet()) {
+            int time = 23 * 60 + 59;
+            int diffTime = time - entry.getValue();
+            outMap.put(entry.getKey(), outMap.getOrDefault(entry.getKey(), 0) + diffTime);
         }
-
-        List<Car> answer = new ArrayList<>();
-        for (Map.Entry<String, Integer> entry : time.entrySet()) {
-            String number = entry.getKey();
-            int nowTime = entry.getValue();
-            int price = fees[1];
-            if (nowTime > fees[0]) {
-                price += (int) (Math.ceil((double) (nowTime - fees[0]) / fees[2]) * fees[3]);
-            }
-            answer.add(new Car(number, price));
+        
+        for (Map.Entry<String, Integer> entry : outMap.entrySet()) {
+            int fee = getFee(fees, entry.getValue());
+            answer.put(entry.getKey(), answer.getOrDefault(entry.getKey(), 0) + fee);
         }
-
-        answer.sort(Comparator.comparing(o -> o.number));
-
-        return answer.stream().mapToInt(i -> i.price).toArray();
+        
+        List<String[]> result = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : answer.entrySet()) {
+            result.add(new String[] {entry.getKey(), "" + entry.getValue()});
+        }
+        Collections.sort(result, (o1, o2) -> o1[0].compareTo(o2[0]));
+        
+        int[] ret = new int[result.size()];
+        for(int idx=0; idx<ret.length; idx++) {
+            ret[idx] = Integer.parseInt(result.get(idx)[1]);
+        }
+        
+        return ret;
     }
-
-    class Car {
-        String number;
-        int price;
-
-        public Car(String number, int price) {
-            this.number = number;
-            this.price = price;
+    
+    int getFee(int[] fees, int time) {
+        if(time <= fees[0]) {
+            return fees[1];
         }
+        return fees[1] + (((time - fees[0] + fees[2] - 1)/fees[2]) * fees[3]);
     }
 }
