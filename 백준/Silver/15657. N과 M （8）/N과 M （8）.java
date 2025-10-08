94a35739c3c470dfc391c5928e5a50d4a317ecfa
taeho99 <70526479+taeho99@users.noreply.github.com>
2025-10-08 23:49:31 +0900

    [Silver III] Title: N과 M (8), Time: 104 ms, Memory: 18240 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.java
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -5,44 +5,40 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] arr, num;
-    static boolean[] visited;
-    static int n, m;
-    static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        arr = new int[m];
-        num = new int[n];
-        visited = new boolean[n];
+	static int elementSize, selectSize;
+	static int[] elementList, selectList;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++) {
-            num[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(num);
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		elementSize = Integer.parseInt(st.nextToken());
+		selectSize = Integer.parseInt(st.nextToken());
 
-        dfs(0, 0);
-        System.out.print(sb);
-    }
+		elementList = new int[elementSize];
+		selectList = new int[selectSize];
+		st = new StringTokenizer(br.readLine());
+		for (int idx = 0; idx < elementSize; idx++) {
+			elementList[idx] = Integer.parseInt(st.nextToken());
+		}
 
-    private static void dfs(int depth, int at) {
-        if(depth == m) {
-            for (int i : arr) {
-                sb.append(i).append(' ');
-            }
-            sb.append('\n');
-            return;
-        }
-        for(int i=at; i<n; i++) {
-//            if(!visited[i]) {
-                visited[i] = true;
-                arr[depth] = num[i];
-                dfs(depth+1, i);
-//                visited[i] = false;
-//            }
-        }
-    }
+		Arrays.sort(elementList);
+		dfs(0, 0);
+		System.out.print(sb);
+	}
+
+	private static void dfs(int depth, int at) {
+		if (depth == selectSize) {
+			for (int select : selectList) {
+				sb.append(select).append(' ');
+			}
+			sb.append('\n');
+			return;
+		}
+
+		for(int idx=at; idx<elementSize; idx++) {
+			selectList[depth] = elementList[idx];
+			dfs(depth+1, idx);
+		}
+	}
 }
