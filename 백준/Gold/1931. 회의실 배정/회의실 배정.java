f7bd19b3aa5607fbda033f18caf3ff261c5f666e
taeho99 <70526479+taeho99@users.noreply.github.com>
2025-10-10 20:10:46 +0900

    [Gold V] Title: 회의실 배정, Time: 572 ms, Memory: 49532 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int size = Integer.parseInt(br.readLine());
+		List<int[]> list = new ArrayList<>();
+		while(size-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			list.add(new int[] {start, end});
+		}
+
+		Collections.sort(list, (o1, o2) -> {
+			if(o1[1] == o2[1]) {
+				return Integer.compare(o1[0], o2[0]);
+			}
+			return Integer.compare(o1[1], o2[1]);
+		});
+		int time = 0, result = 0;
+		for (int[] ints : list) {
+			if(time <= ints[0]) {
+				result++;
+				time = ints[1];
+			}
+		}
+		System.out.println(result);
+	}
+}
