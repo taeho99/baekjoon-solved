a9cbd228f91f25843f134193c49fc791723cc1a9
taeho99 <70526479+taeho99@users.noreply.github.com>
2025-10-10 20:47:35 +0900

    [Silver I] Title: 신입 사원, Time: 1676 ms, Memory: 304700 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -2,46 +2,41 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int t = Integer.parseInt(br.readLine());
-        while(t-- > 0) {
-            int n = Integer.parseInt(br.readLine());
-            ArrayList<Pair> list = new ArrayList<>();
-            for(int i=0; i<n; i++) {
-                st = new StringTokenizer(br.readLine());
-                list.add(new Pair(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
-            }
-            Collections.sort(list);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
 
-            int result = 1;
-            int min = list.get(0).b;
+		int T = Integer.parseInt(br.readLine());
+		while(T-- > 0) {
+			int size = Integer.parseInt(br.readLine());
+			List<int[]> list = new ArrayList<>();
+			while(size-- > 0) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
 
-            for(int i=1; i<list.size(); i++) {
-                if(list.get(i).b < min) {
-                    result++;
-                    min = list.get(i).b;
-                }
-            }
-            System.out.println(result);
-        }
-    }
-    static class Pair implements Comparable<Pair> {
-        int a, b;
+				list.add(new int[] {a, b});
+			}
+			Collections.sort(list, (o1, o2) -> Integer.compare(o1[1], o2[1]));
 
-        public Pair(int a, int b) {
-            this.a = a;
-            this.b = b;
-        }
-
-        @Override
-        public int compareTo(Pair o) {
-            return Integer.compare(this.a, o.a);
-        }
-    }
+			int people = 1;
+			int maxScore = list.get(0)[0];
+			for(int idx=1; idx<list.size(); idx++) {
+				int[] now = list.get(idx);
+				if(now[0] < maxScore) {
+					people++;
+					maxScore = now[0];
+				}
+			}
+			sb.append(people).append('\n');
+		}
+		System.out.print(sb);
+	}
 }
