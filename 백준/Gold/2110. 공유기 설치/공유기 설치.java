99080f47e6411fe8aba80e4b8a9499256f1b6cdd
taeho99 <70526479+taeho99@users.noreply.github.com>
2025-10-17 18:48:10 +0900

    [Gold IV] Title: 공유기 설치, Time: 320 ms, Memory: 34556 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,47 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, c;
-    static int[] arr;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int homeCnt = Integer.parseInt(st.nextToken());
+		int routerCnt = Integer.parseInt(st.nextToken());
 
-        arr = new int[n];
-        for(int i=0; i<n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
+		List<Integer> home = new ArrayList<>();
+		while(homeCnt-- > 0) {
+			home.add(Integer.parseInt(br.readLine()));
+		}
+		Collections.sort(home);
+		int lo = 1, hi = home.get(home.size()-1) - home.get(0) + 1;
 
-        Arrays.sort(arr);
+		while(lo+1 < hi) {
+			int mid = (lo+hi) / 2;
+			if(check(home, routerCnt, mid)) hi = mid;
+			else lo = mid;
+		}
+		System.out.println(lo);
+	}
 
-        int lo = 1;
-        int hi = arr[n-1] - arr[0] + 1;
-
-        while(lo+1 < hi) {
-            int mid = (lo+hi)/2;
-
-            if(check(mid)) hi = mid;
-            else lo = mid;
-        }
-        System.out.println(lo);
-    }
-
-    private static boolean check(int mid) {
-        int count = 1;
-        int last = arr[0];
-
-        for(int i=1; i<n; i++) {
-            if(arr[i] - last >= mid) {
-                count++;
-                last = arr[i];
-            }
-        }
-        return count < c;
-    }
+	private static boolean check(List<Integer> home, int routerCnt, int mid) {
+		int cnt = 1;
+		int last = home.get(0);
+		for (Integer pos : home) {
+			if(pos - last >= mid) {
+				cnt++;
+				last = pos;
+			}
+		}
+		return cnt < routerCnt;
+	}
 }
